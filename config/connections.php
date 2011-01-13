bf751122702b366e90c5c1c98808c4fb113668ab
juliogreff <juliogreff@gmail.com>
2011-01-13 17:38:56 +0100

    Configs and default directories

--- config/connections.php
+++ config/connections.php
@@ -3,7 +3,7 @@
 Connection::add(array(
     'development' => array(
         'driver' => 'MySql',
-        'host' => 'localhost',
+        'host' => '127.0.0.1',
         'user' => 'root',
         'password' => '',
         'database' => 'mobuilder',
