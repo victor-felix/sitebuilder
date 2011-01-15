ab26e9d2eb5a9cb42cadb3ff07dac449252b405a
juliogreff <juliogreff@gmail.com>
2011-01-15 14:11:43 +0100

    Fixed api routes - calling explicit actions correctly

--- config/routes.php
+++ config/routes.php
@@ -6,12 +6,4 @@ Mapper::prefix('api');
 
 
 Mapper::connect('/api/:fragment/([\w\d_-]+)(\.[\w]+)?', '/api/$2/index/$1$3');
-Mapper::connect('/api/:fragment/:fragment/:fragment/:any', '/api/$2/$3/$1/$4');
-
-
-
-// Mapper::connect('/api/:fragment/:fragment(\.[\w+])', '/api/$2/index/$1$3');
-// Mapper::connect('/api/([\w\d._-]+)/:any(\.[\w+])?', '/api/$2/$1$3');
-// Mapper::connect('/api/([\w\d._-]+)/:any(\.[\w+])?', '/api/$2/$1$3');
-// Mapper::connect('/api/([\w\d._-]+)/:any(\.[\w+])?', '/api/$2/$1$3');
-// Mapper::connect('/api/([\w\d._-]+)/:any(\.[\w+])?', '/api/$2/$1$3');
+Mapper::connect('/api/:fragment/:fragment/:fragment(:any)?', '/api/$2/$3/$1$4');
