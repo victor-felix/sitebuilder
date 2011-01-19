04f578400399c64a9cf579566b899c038b0d191a
Rafael Marin <rafael@rafaelmarin.net>
2011-01-19 20:32:15 +0100

    added full finish page

--- app/views/sites/finished.htm.php
+++ app/views/sites/finished.htm.php
@@ -8,12 +8,12 @@ $this->showTitle = false;
     )); ?>
     <h2>Parabéns! Seu site <strong>mobi</strong> já pode ser acessado em <?php echo $this->html->link('http://google.com') ?></h2>
     <div class="next-steps">
-        <h3></h3>
-        <p></p>
+        <h3>Próximas etapas</h3>
+        <p>Seu site mobi já está online, mas você ainda pode continuar a melhorá-lo.</p>
         <ul>
-            <li><?php echo $this->html->link('Adicione o cardápio de seu restaurante ›', '') ?></li>
-            <li><?php echo $this->html->link('Forneça mais detalhes sobre sua empresa ›', '') ?></li>
-            
+            <li><?php echo $this->html->link('Adicione o cardápio de seu restaurante ›', '/categories') ?></li>
+            <li><?php echo $this->html->link('Forneça mais detalhes sobre sua empresa ›', '/settings') ?></li>
         </ul>
+        <div class="clear"></div>
     </div>
 </div>
