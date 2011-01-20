e95c8482930ce388a8d62115965ed3bb52e73b83
Rafael Marin <rafael@rafaelmarin.net>
2011-01-20 13:14:06 +0100

    improved login page

---
+++ app/views/layouts/login.htm.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php echo $this->html->charset() ?>
+        <title><?php echo $this->pageTitle ?></title>
+
+		<?php echo $this->html->stylesheet('register', 'uikit'); ?>
+    </head>
+    <body>
+	
+		<div id="header">
+			<?php echo $this->html->link($this->html->image('layout/logo.png', array('class'=>'MeuMobi')), '/', array('class'=>'logo')); ?>
+	    </div>
+	    
+	    
+	    <div id="content">
+	        <?php if(!isset($this->showTitle) || (isset($this->showTitle) && $this->showTitle)): ?>
+    	    <div class="head">
+    	        <h1>crie seu mobi</h1>
+    	        <ul class="steps">
+    	            <li class="current">informações pessoais</li>
+    	            <li>informações do negócio</li>
+    	            <li>customização e logotipo</li>
+    	        </ul>
+    	        <div class="clear"></div>
+    	    </div>
+    	    <?php endif ?>
+    	    
+            <?php echo $this->contentForLayout ?>
+        </div>
+        
+        <?php echo $this->element("layouts/footer") ?>
+        
+        <?php echo $this->html->script('jquery', 'main') ?>
+    </body>
+</html>
--- app/views/users/login.htm.php
+++ app/views/users/login.htm.php
@@ -1,11 +1,32 @@
-<?php echo $this->form->create('/users/login') ?>
-    <?php echo $this->form->input('email', array(
-        'label' => __('E-Mail')
-    )) ?>
+<?php $this->layout = "register";
+$this->showTitle = false; ?>
 
-    <?php echo $this->form->input('password', array(
-        'label' => __('Senha')
-    )) ?>
-
-
-<?php echo $this->form->close('Login') ?>
+<?php echo $this->form->create('/users/login', array(
+    'class' => 'form-register'
+)) ?>
+<fieldset>
+    <h2>login</h2>
+    <div class="field-group">
+        
+        <div class="form-grid-220 first">
+        <?php echo $this->form->input('email', array(
+            'label' => __('E-Mail'),
+            'class' => 'ui-text'
+        )) ?>
+        </div>
+        
+        <div class="form-grid-220 first">
+        <?php echo $this->form->input('password', array(
+            'label' => __('Senha'),
+            'class' => 'ui-text'
+        )) ?>
+        </div>
+    </div>
+</fieldset>
+<fieldset class="actions">
+    <?php echo $this->form->submit('Login', array(
+        'class' => 'ui-button red large',
+        'style' => 'margin-right: 415px'
+    ))?>
+</fieldset>
+<?php echo $this->form->close() ?>
