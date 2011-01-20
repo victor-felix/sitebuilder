8daff312b0f5af74550d270dbb1ba220efb7fd38
Rafael Marin <rafael@rafaelmarin.net>
2011-01-20 13:22:36 +0100

    improved login page

--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,8 @@ Mapper::connect("/settings", "/sites/edit");
 Mapper::connect("/settings/account", "/users/edit");
 Mapper::connect("/settings/customize", "/sites/customize_edit");
 Mapper::connect("/register", "/users/register");
+Mapper::connect("/login", "/users/login");
+Mapper::connect("/logout", "/users/logout");
 Mapper::connect("/dashboard", "/categories");
 
 Mapper::connect('/api/:fragment', '/api/home/index/$1$3');
