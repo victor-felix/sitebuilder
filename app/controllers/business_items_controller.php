2e4032c674b1969dbec5606a026368a7b835d287
Iraê <irae@irae.pro.br>
2011-01-28 00:47:06 -0200

    Redirecting to a item breaks ajax and animations. Only when upload is assync we can restore this lines

--- app/controllers/business_items_controller.php
+++ app/controllers/business_items_controller.php
@@ -24,7 +24,8 @@ class BusinessItemsController extends AppController {
                 }
                 else {
                     Session::writeFlash('success', __('Item adicionado com sucesso.'));
-                    $this->redirect('/business_items/index/' . $business_item->parent_id);
+                    // $this->redirect('/business_items/index/' . $business_item->parent_id);
+                    $this->redirect('/categories');
                 }
             }
         }
@@ -48,7 +49,8 @@ class BusinessItemsController extends AppController {
                 }
                 else {
                     Session::writeFlash('success', __('Item editado com sucesso.'));
-                    $this->redirect('/business_items/index/' . $business_item->parent_id);
+                    // $this->redirect('/business_items/index/' . $business_item->parent_id);
+                    $this->redirect('/categories');
                 }
             }
         }
