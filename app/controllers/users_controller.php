0702e46992b4a63e28f2ac05631995bac3565ded
juliogreff <juliogreff@gmail.com>
2011-04-11 13:41:11 -0300

    Fixed login on segment in which the user has no site

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -38,7 +38,7 @@ class UsersController extends AppController {
     public function login() {
         if(!empty($this->data)) {
             $user = Auth::identify($this->data);
-            if($user) {
+            if($user && $user->hasSiteInSegment(MeuMobi::$segment)) {
                 Auth::login($user);
 
                 if(!($location = Session::flash('Auth.redirect'))) {
