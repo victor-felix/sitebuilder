15b6489578a7916c68f2594e0766a51e746799dd
juliogreff <juliogreff@gmail.com>
2011-04-26 02:01:29 -0300

    Finished importing old Articles methods

--- app/models/articles.php
+++ app/models/articles.php
@@ -31,6 +31,25 @@ class Articles extends BusinessItems {
         return array('title', 'description', 'author');
     }
 
+    public function topBySlug($slug) {
+        $feed = Model::load('Sites')->firstBySlug($slug)->feed();
+        return $this->topByFeedId($feed->id);
+    }
+
+    public function topByFeedId($feed_id) {
+        return $this->all(array(
+            'table' => array('a' => $this->table()),
+            'fields' => 'a.*',
+            'joins' => 'JOIN business_items_values AS v ' .
+                'ON a.id = v.item_id',
+            'conditions' => array(
+                'v.field' => 'feed_id',
+                'v.value' => $feed_id
+            ),
+            'limit' => Config::read('Articles.limit')
+        ));
+    }
+
     public function articleExists($feed_id, $guid) {
         $guid = $this->filterGuid($guid);
         $model = Model::load('BusinessItemsValues');
@@ -53,7 +72,101 @@ class Articles extends BusinessItems {
             'pubdate' => $item->get_date('Y-m-d H:i:s')
         );
 
-        $this->save($article);
+        try {
+            $this->begin();
+
+            $this->save($article);
+            $images = $this->getImages($item);
+            foreach($images as $image) {
+                $image = $this->getImageUrl($image, $article['guid']);
+                $result = Model::load('Images')->download($this, $image, array(
+                    'url' => $image
+                ));
+
+                if($result) {
+                    $log->logInfo('Downloaded image "%s" to "%s"', $image, $result);
+                }
+                else {
+                    $log->logInfo('Image "%s" could not be found', $image, $result);
+                }
+            }
+
+            $this->commit();
+
+            $log->logInfo('Imported article "%s"', $article['guid']);
+        }
+        catch(Exception $e) {
+            $this->rollback();
+
+            $log->logInfo('Could not import article "%s". Exception: %s', $article['guid'], (string) $e);
+
+            return false;
+        }
+    }
+
+    protected function getImages($item) {
+        $images = $this->getEnclosureImages($item);
+        if(empty($images)) {
+            $images = $this->getContentImages($item);
+        }
+
+        foreach($images as $k => $image) {
+            if($this->isBlackListed($image)) {
+                unset($images[$k]);
+            }
+        }
+
+        return $images;
+    }
+
+    protected function getContentImages($item) {
+        $content = str_get_html($item->get_content());
+        $links = $content->find('a[rel=lightbox]');
+
+        $images = array();
+
+        foreach($links as $link) {
+            $images []= $link->href;
+        }
+
+        return $images;
+    }
+
+    protected function getEnclosureImages($item) {
+        $images = array();
+        $enclosures = $item->get_enclosures();
+        if(is_null($enclosures)) return $images;
+
+        foreach($enclosures as $enclosure) {
+            if($enclosure->get_medium() == 'image') {
+                $images []= $enclosure->get_link();
+            }
+        }
+
+        return $images;
+    }
+
+    protected function getImageUrl($url, $article) {
+        if(Mapper::isRoot($url)) {
+            $domain = parse_url($article, PHP_URL_HOST);
+            $url = 'http://' . $domain . $url;
+        }
+        else if(preg_match('%^(http://download.rj.gov.br/imagens/\d+/\d+/\d+.jpg)%', $url, $output)) {
+            return $output[0];
+        }
+
+        return $url;
+    }
+
+    protected function isBlackListed($link) {
+        foreach(self::$blacklist as $i) {
+            $pattern = preg_quote($i);
+            if(preg_match('%' . $pattern . '%', $link)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     protected function getPurifier() {
