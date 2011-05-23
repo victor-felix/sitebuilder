60573017f36258c7e7ce9dc02268e711558c4f0e
juliogreff <juliogreff@gmail.com>
2011-05-23 19:54:39 -0300

    Added order to api/news

--- app/controllers/api/news_controller.php
+++ app/controllers/api/news_controller.php
@@ -6,8 +6,11 @@ class NewsController extends ApiController {
     protected $uses = array('Articles');
 
     public function api_index($slug = null) {
-        $news = $this->Articles->allByParentIdAndSiteId(0, $this->site->id, array(
-            'limit' => $this->param('limit', 10)
+        $news = $this->Articles->allOrdered(array(
+            'conditions' => array(
+                'site_id' => $this->site->id,
+                'parent_id' => 0
+            )
         ));
         $this->respondToJSON(array(
             'articles' => $news
