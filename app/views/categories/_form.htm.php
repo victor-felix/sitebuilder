857256ca53033035ab40f50f80209857166d9a52
Iraê <irae@irae.pro.br>
2011-05-24 02:24:56 -0300

    Working edit category view.

---
+++ app/views/categories/_form.htm.php
@@ -0,0 +1,69 @@
+
+<?php echo $this->form->create($action, array(
+    'class' => 'form-edit',
+    'object' => $category
+)) ?>
+
+<fieldset>
+    <h2><?php echo __('categoria') ?></h2>
+    <div class="field-group">
+        <div class="form-grid-460 first">
+            <?php echo $this->form->input('title', array(
+                'label' => __('Nome da categoria'),
+                'class' => 'ui-text large'
+            )) ?>
+        </div>
+
+        <?php if($site->hasManyTypes()): ?>
+            <div class="form-grid-460 first">
+                <?php echo $this->form->input('type', array(
+                    'label' => __('Tipo'),
+                    'type' => 'select',
+                    'class' => 'ui-select large',
+                    'options' => Segments::listItemTypesFor($site->segment)
+                )) ?>
+            </div>
+        <?php endif ?>
+
+        <div class="form-grid-460 populate-fields">
+            <label><?php echo __('Tipo de categoria'); ?></label>
+            <?php echo $this->form->input("populate", array(
+                "type" => "radio",
+                "options" => array(
+                    "auto" => "Automática",
+                    "manual" => "Manual"
+                )
+            )); ?>
+            <small><?php echo __('Categorias automáticas permitem a importação automática de conteúdo a partir de feeds RSS.'); ?></small>
+            <small><?php echo __('Categorias manuais permitem a edição manual de todos os itens associados.'); ?></small>
+        </div>
+
+        <div class="form-grid-460 first">
+            <?php echo $this->form->input('feed', array(
+                'label' => __('URL do Feed'),
+                'class' => 'ui-text large'
+            )) ?>
+        </div>
+
+        <div class="form-grid-460 first">
+            <?php echo $this->form->input('visibility', array(
+                'type' => 'checkbox',
+                'label' => __('Visibilidade')
+            )) ?>
+            <label for="FormVisibility" class="checkbox"><?php echo __('Esta categoria está visível e disponível para os usuários do site mobile') ?></label>
+        </div>
+
+        <?php echo $this->form->input('parent_id', array(
+            'type' => 'hidden',
+            'value' => $parent->id
+        )) ?>
+    </div>
+</fieldset>
+
+<fieldset class="actions">
+    <?php echo $this->form->submit(__('Salvar'), array(
+        'class' => 'ui-button red larger'
+    )) ?>
+</fieldset>
+
+<?php echo $this->form->close() ?>
