c942e2c8f99a2109422c9e671c2fbc383c218936
Victor Dias <victor.dias@ks359225.kimsufi.com>
2011-06-08 14:28:53 +0200

    add thumb resize for links icons, 30x30

--- config/app/resizes.php
+++ config/app/resizes.php
@@ -1,4 +1,4 @@
 <?php
 
 Config::write('SiteLogos.resizes', array('200x200'));
-Config::write('BusinessItems.resizes', array('80x80#'));
+Config::write('BusinessItems.resizes', array('80x80#', '30x30#'));
