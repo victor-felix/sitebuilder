5eaac47b9864e569f176a677c18f83bc55c4ded8
juliogreff <juliogreff@gmail.com>
2011-07-06 18:45:12 -0300

    Fixing notice in i18n initializer

--- config/initializers/i18n.php
+++ config/initializers/i18n.php
@@ -1,12 +1,14 @@
 <?php
 
-$languages = strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], ';', true);
-$languages = explode(',', $languages);
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    $languages = strstr($_SERVER['HTTP_ACCEPT_LANGUAGE'], ';', true);
+    $languages = explode(',', $languages);
 
-while(list($i, $language) = each($languages)) {
-    if(Filesystem::exists(I18n::path() . '/' . $language . '.yaml')) {
-        I18n::locale($language);
-        break;
+    while(list($i, $language) = each($languages)) {
+        if(Filesystem::exists(I18n::path() . '/' . $language . '.yaml')) {
+            I18n::locale($language);
+            break;
+        }
     }
 }
 
