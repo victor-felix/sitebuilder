bc9421838d97e14e96058b663267862133a7de26
juliogreff <juliogreff@gmail.com>
2011-07-12 19:39:39 -0300

    Fixed main domain api error

--- public/meumobi/index.php
+++ public/meumobi/index.php
@@ -1,20 +1,3 @@
 <?php
 
-try {
-    require dirname(dirname(dirname(__FILE__))) . '/config/bootstrap.php';
-    require 'config/settings.php';
-    require 'config/connections.php';
-    require 'config/routes.php';
-
-    echo Dispatcher::dispatch();
-}
-catch(Exception $e) {
-    Debug::log((string) $e);
-
-    if(Config::read('Debug.showErrors')) {
-        echo '<pre>', $e, '</pre>';
-    }
-    else {
-        // @todo do something to prevent white screen of death
-    }
-}
+require dirname(dirname(__FILE__)) . '/dispatch.php';
