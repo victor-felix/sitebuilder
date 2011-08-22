44771ca1efa4f41193c2f7a0677ff29aa5ac4af9
juliogreff <juliogreff@gmail.com>
2011-08-22 19:22:04 -0300

    Updating API urls

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,4 +5,4 @@ ini_set('display_errors', 'On');
 
 Config::write('Debug.showErrors', true);
 
-Config::write('Themes.url', 'http://meu-cloud-db.int-meumobi.com/groups/4e458e1780623362a4000001/configs.json');
+Config::write('Themes.url', 'http://meu-cloud-db.int-meumobi.com/configs.json');
