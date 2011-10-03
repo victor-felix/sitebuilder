b77488de88bbe661fa4f5f29b3d83f387cb1c1dc
juliogreff <juliogreff@gmail.com>
2011-10-03 08:26:58 -0300

    Added geocoding

--- app/models/business_items.php
+++ app/models/business_items.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once 'lib/bbcode/Decoda.php';
+require_once 'lib/geocoding/GoogleGeocoding.php';
 
 class BusinessItems extends AppModel {
     protected $table = 'business_items';
-    protected $beforeSave = array('setSiteValues', 'getOrder');
+    protected $geocoding = false;
+    protected $beforeSave = array('setSiteValues', 'getOrder', 'getLatLng');
     protected $afterSave = array('saveItemValues', 'saveImages');
     protected $beforeDelete = array('deleteValues', 'deleteImages');
     protected $validates = array(
@@ -320,5 +322,66 @@ class BusinessItems extends AppModel {
             )),
             'conditions' => $conditions
         ));
+    }
+
+    public function nearest($category_id, $lat, $lng) {
+        $items = $this->allByParentId($category_id);
+        $nearest = array();
+
+        foreach($items as $item) {
+            $distance = $item->distance($lat, $lng);
+            if($distance < 0.5) {
+                $nearest []= $item;
+            }
+        }
+
+        return $nearest;
+    }
+
+    public function area($category_id, $ne_lat, $ne_lng, $sw_lat, $sw_lng) {
+        $items = $this->allByParentId($category_id);
+        $area = array();
+
+        foreach($items as $item) {
+            if($item->inside($ne_lat, $ne_lng, $sw_lat, $sw_lng)) {
+                $area []= $item;
+            }
+        }
+
+        return $area;
+    }
+
+    protected function distance($lat, $lng) {
+        $values = $this->values();
+        if(!empty($values->lat) && !empty($values->lng)) {
+            $distance = sqrt(pow($lat - $values->lat, 2) + pow($lng - $values->lng, 2));
+            return $distance;
+        }
+    }
+
+    protected function inside($ne_lat, $ne_lng, $sw_lat, $sw_lng) {
+        $s = $this->values();
+        return ($s->lat >= $sw_lat && $s->lat <= $ne_lat) &&
+            ($s->lng >= $sw_lng && $s->lng <= $ne_lng);
+    }
+
+    protected function getLatLng($data) {
+        if($this->geocoding) {
+            if(!empty($data['address'])) {
+                try {
+                    $geocode = GoogleGeocoding::geocode($data['address']);
+                    $location = $geocode->results[0]->geometry->location;
+                    $data['lat'] = $location->lat;
+                    $data['lng'] = $location->lng;
+
+                    return $data;
+                }
+                catch(Exception $e) {}
+            }
+
+            $data['lat'] = $data['lng'] = '';
+        }
+
+        return $data;
     }
 }
--- app/models/events.php
+++ app/models/events.php
@@ -3,7 +3,7 @@
 require_once 'app/models/business_items.php';
 
 class Events extends BusinessItems {
-    protected $beforeSave = array('setSiteValues', 'getOrder', 'getLatLng');
+    protected $geocoding = true;
     protected $typeName = 'Event';
     protected $fields = array(
         'lat' => array(),
@@ -36,64 +36,5 @@ class Events extends BusinessItems {
 
     public function fields() {
         return array('title', 'description', 'address', 'contact', 'date', 'hour');
-    }
-
-    public function nearest($category_id, $lat, $lng) {
-        $items = $this->allByParentId($category_id);
-        $nearest = array();
-
-        foreach($items as $item) {
-            $distance = $item->distance($lat, $lng);
-            if($distance < 0.5) {
-                $nearest []= $item;
-            }
-        }
-
-        return $nearest;
-    }
-
-    public function area($category_id, $ne_lat, $ne_lng, $sw_lat, $sw_lng) {
-        $items = $this->allByParentId($category_id);
-        $area = array();
-
-        foreach($items as $item) {
-            if($item->inside($ne_lat, $ne_lng, $sw_lat, $sw_lng)) {
-                $area []= $item;
-            }
-        }
-
-        return $area;
-    }
-
-    protected function distance($lat, $lng) {
-        $values = $this->values();
-        if(!empty($values->lat) && !empty($values->lng)) {
-            $distance = sqrt(pow($lat - $values->lat, 2) + pow($lng - $values->lng, 2));
-            return $distance;
-        }
-    }
-
-    protected function inside($ne_lat, $ne_lng, $sw_lat, $sw_lng) {
-        $s = $this->values();
-        return ($s->lat >= $sw_lat && $s->lat <= $ne_lat) &&
-            ($s->lng >= $sw_lng && $s->lng <= $ne_lng);
-    }
-
-    protected function getLatLng($data) {
-        if(!empty($data['address'])) {
-            try {
-                $geocode = GoogleGeocoding::geocode($data['address']);
-                $location = $geocode->results[0]->geometry->location;
-                $data['lat'] = $location->lat;
-                $data['lng'] = $location->lng;
-
-                return $data;
-            }
-            catch(Exception $e) {}
-        }
-
-        $data['lat'] = $data['lng'] = '';
-
-        return $data;
     }
 }
