5d436ab98ef0362b51c08d700f20ef424e5bce51
juliogreff <juliogreff@gmail.com>
2011-10-16 22:32:10 -0200

    fixes

--- script/migrate.php
+++ script/migrate.php
@@ -1,6 +1,6 @@
 <?php
 
-require dirname(dirname(__DIR__)_ . '/config/bootstrap.php';
+require dirname(dirname(__DIR__)) . '/config/bootstrap.php';
 require 'config/settings.php';
 require 'config/connections.php';
 
