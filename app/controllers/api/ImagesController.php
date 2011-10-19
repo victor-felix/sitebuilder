582143220f1b7947604aead721b8619a8e5ac9f0
juliogreff <juliogreff@gmail.com>
2011-10-18 23:00:33 -0200

    Fixed inconsistent image upload in API

--- app/controllers/api/ImagesController.php
+++ app/controllers/api/ImagesController.php
@@ -22,7 +22,7 @@ class ImagesController extends ApiController {
             '_id' => $this->request->params['item_id']
         )));
 
-        $data = $this->request->data['image'];
+        $data = array_unset($this->request->data, 'image');
         $visible = isset($this->request->data['visible']) ? $this->request->data['visible'] : 0;
         $params = array_merge($this->request->data, compact('visible'));
         $image = Model::load('Images')->upload($item, $data, $params);
