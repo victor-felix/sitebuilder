9ebe7f905ed98cfbecf287392361d5576b13b4b4
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2011-12-09 10:13:56 -0300

    upload error handled

--- app/views/images/add.htm.php
+++ app/views/images/add.htm.php
@@ -5,9 +5,12 @@
     <title></title>
 </head>
 <body id="add.htm">
+	
     <script type="text/javascript" charset="utf-8">
+    
         var context = window.parent;
         var elm = window.parent.document.getElementById("upload_" + <?php echo $timestamp ?>);
+	<?php if($image): ?>
         elm.className = elm.className.replace(/\b(wait|default)\b/g,"")+" done ";
         elm.getElementsByTagName("a")[0].href = "<?php echo '/images/delete/' . $image->id ?>";
         var inputs = elm.getElementsByTagName("input");
@@ -18,7 +21,12 @@
 						input.id = input.id.replace("ID", <?php echo $image->id ?>);
 					};
 				};
-        elm.style.backgroundImage = 'url("<?php echo $image->link('139x139') ?>")'
+        elm.style.backgroundImage = 'url("<?php echo $image->link('139x139') ?>")';
+	<?php else: ?>
+        context.$(elm).remove();
+        alert("sorry, we had a upload problem");
+    <?php endif;?>
     </script>
+    
 </body>
 </html>
