abb45a54a968cd4fb81b892af4685d0db001115d
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-02-10 10:56:47 -0300

    fix geocoded values

--- db/migrations/20120131082655_update_no_geo_value.php
+++ db/migrations/20120131082655_update_no_geo_value.php
@@ -3,7 +3,9 @@ use lithium\data\Connections;
 class UpdateNoGeoValue{
     public static function migrate($connection) {
     	$collection = Connections::get('default')->connection->items;
-	$newdata = array('$set' => array("geo" => 0));
-	$collection->update(array("geo" => array(0,0)), $newdata, array('multiple' => true));
+		$conditions['$or'][] = array("geo" => array(0,0));
+		$conditions['$or'][] = array("geo" => array());
+		$newdata = array('$set' => array("geo" => 0));
+		$collection->update($conditions , $newdata, array('multiple' => true));
     }
 }
