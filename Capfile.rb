3dea24b691d5a5c8aee14d392e8b4095d5b92187
juliogreff <juliogreff@gmail.com>
2012-03-05 10:25:43 -0300

    Fixed Capfile

--- Capfile.rb
+++ Capfile.rb
@@ -34,12 +34,12 @@ namespace :deploy do
   end
 
   task :platform_check do
-    run "#{release_path}/script/check_platform.php"
+    run "#{release_path}/meu-site-builder/script/check_platform.php"
   end
 
   namespace :db do
     task :migrate do
-      run "#{release_path}/script/migrate.php"
+      run "#{release_path}/meu-site-builder/script/migrate.php"
     end
   end
 end
