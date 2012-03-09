786b7ea9c14d1dd667f9239b6c6f5a157d8ab244
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-03-09 09:29:31 -0300

    geocode item

--- lib/utils/jobs/Geocode.php
+++ lib/utils/jobs/Geocode.php
@@ -4,58 +4,69 @@ require_once 'lib/geocoding/GoogleGeocoding.php';
 class Geocode
 {
     const GEOCODE_LIMIT = 10;
-    
+
+    public function __construct()
+    {
+        set_time_limit(0);
+    }
     public function run()
     {
         $page = 1;
-        while ($jobs = $this->getJobs($page)) {   
+        $jobsToRemove = array();
+
+        while ($jobs = $this->getJobs($page)) {
             foreach ($jobs as $job) {
+                //echo $job->_id,"\n"; continue;
                 $classname = '\app\models\items\\' .
                 Inflector::camelize($job->params->type);
                 $item = $classname::first(array(
-                		'conditions' => array(
-                				'_id' => $job->params->item_id,
-                		),
+                    'conditions' => array(
+                        '_id' => $job->params->item_id,
+                    ),
                 ));
-                
+
                 if (!$item) {
                     continue;
                 }
-                
+
                 $geocode = GoogleGeocoding::geocode($item->address);
                 switch ($geocode->status) {
                     case 'OK' :
                             $location = $geocode->results[0]->geometry->location;
                             $item->geo = array($location->lng, $location->lat);
                             $item->save();
+                            $jobsToRemove[] = (string)$job->_id;
                         break;
-                    case '' :
+                    case 'OVER_QUERY_LIMIT' :
                         break 3;
                     default :
-                        continue;
+                            $jobsToRemove[] = (string)$job->_id;
                 }
-            }
-            
-            if($page > 10){
+            }//end foreach
+
+            if ($page > 10) {
                 break;
             }
             $page++;
-        }
+        }//end while
+
+        return $this->removeJobs($jobsToRemove);
     }
-    
-    protected function getJobs($page) {
+
+ protected function removeJobs($jobsIds)
+    {
+        return \app\models\Jobs::remove(array('_id' => $jobsIds));
+    }
+
+    protected function getJobs($page) 
+    {
         $jobs = \app\models\Jobs::all(array(
-    		'conditions' => array('type' => 'geocode'),
-    		'order' => 'modified',
+            'conditions' => array('type' => 'geocode'),
+            'order' => 'modified',
             'limit' => self::GEOCODE_LIMIT,
             'page' => $page,
         ));
-        
+
         return count($jobs) ? $jobs : false;
-    }
-    
-    public function geocodeItem() 
-    {
-        
     }
 }
