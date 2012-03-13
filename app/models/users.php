15952abe1e81e3e9f38fa4f308c81fc6ac5a6f99
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-03-13 16:29:40 -0300

    add invite and confirm invite to users model

--- app/models/users.php
+++ app/models/users.php
@@ -1,5 +1,5 @@
 <?php
-use lithium\storage\Session;
+use lithium\storage\Session, lithium\util\Validator;
 
 class Users extends AppModel {
 	const CURRENT_SITE = 'User.site';
@@ -28,6 +28,12 @@ class Users extends AppModel {
 		return preg_replace('/,/', ' ', $this->name);
 	}
 
+	public function addSite($site)
+	{
+	    $model = Model::load('UsersSites');
+	    return $model->add($this, $site);
+	}
+	
 	public function site($siteId = false) {
 		$model = Model::load('UsersSites');
 		if ($siteId && $model->check($this->id, $siteId)) {
@@ -109,8 +115,61 @@ class Users extends AppModel {
 			return false;
 		}
 	}
-
-	protected function hashPassword($data) {
+    
+	public function invite($emails)
+	{
+	    $emails = $this->prepareEmails($emails);
+	    $site = $this->site();
+	    foreach ($emails as $email) {
+	        if ($this->inviteToSite($email, $site)) {
+	            $this->sendInviteEmail($email, "Invited by {$this->fullname()}");
+	        }
+	    }
+	}
+	
+	public function confirmInvite($token)
+	{
+	    $invite = \app\models\Invites::first(array(
+	            'conditions' => array('token' => $token,)
+	            ));
+	    if (!$invite) {
+	        return false;
+	    }
+	    
+	    $site = Model::load('sites')->firstById($invite->site_id);
+	    
+	    if ($site && $this->addSite($site)) {
+	        $invite->delete();
+	        return true;
+	    }
+	} 
+	
+	protected function prepareEmails($emails)
+	{
+	    $chars = array("
+	    		", " ", "\n", "\r", "chr(13)", "\t", "\0", "\x0B");
+	    $emails = str_replace($chars, '', (string)$emails);
+	    
+	    return array_filter(explode(',', $emails), function($email) {
+	        return Validator::isEmail($email);
+	    });
+	}
+	
+	protected function inviteToSite($email,$site)
+	{
+	    $data = array(
+    		'site_id' => $site->id,
+    		'host_id' => $this->id,
+    		'email' => $email,
+            'token' => Security::hash($email . time(),'sha1'),
+	    );
+	    
+	    $invite = \app\models\Invites::create($data);
+	    return $invite->save();
+	}
+	
+	protected function hashPassword($data) 
+	{
 		if (array_key_exists ( 'password', $data ) && array_key_exists ( 'confirm_password', $data )) {
 			$password = array_unset ( $data, 'password' );
 			if (! empty ( $password )) {
@@ -122,7 +181,8 @@ class Users extends AppModel {
 		return $data;
 	}
 
-	protected function createToken($data) {
+	protected function createToken($data) 
+	{
 		if (is_null ( $this->id )) {
 			$data ['token'] = $this->newToken ();
 		}
@@ -130,22 +190,26 @@ class Users extends AppModel {
 		return $data;
 	}
 
-	protected function newToken() {
+	protected function newToken() 
+	{
 		return Security::hash ( time (), 'sha1' );
 	}
 
-	protected function removeSites() {
+	protected function removeSites() 
+	{
 		return Model::load ( 'UsersSites' )->onDeleteUser ( $this );
 	}
 
-	protected function createSite($created) {
+	protected function createSite($created) 
+	{
 		if ($created) {
 			$model = Model::load ( 'Sites' );
 			$model->save ( array ('segment' => MeuMobi::segment (), 'slug' => '', 'title' => '' ) );
 		}
 	}
 
-	protected function sendConfirmationMail($created) {
+	protected function sendConfirmationMail($created) 
+	{
 		if ($created && ! Config::read ( 'Mail.preventSending' )) {
 			require_once 'lib/mailer/Mailer.php';
 			$segment = Model::load ( 'Segments' )->firstById ( MeuMobi::segment () );
@@ -155,8 +219,9 @@ class Users extends AppModel {
 		}
 	}
 
-	protected function sendForgottenPasswordMail() {
-		if (! Config::read ( 'Mail.preventSending' )) {
+	protected function sendForgottenPasswordMail() 
+	{
+		if (!Config::read ( 'Mail.preventSending' )) {
 			require_once 'lib/mailer/Mailer.php';
 			$segment = Model::load ( 'Segments' )->firstById ( MeuMobi::segment () );
 
@@ -164,14 +229,33 @@ class Users extends AppModel {
 			$mailer->send ();
 		}
 	}
-
-	protected function authenticate($created) {
+    
+	protected function sendInviteEmail($to, $title, $data = array(), $template = '')
+	{
+	    if (!Config::read ( 'Mail.preventSending' )) {
+	    	require_once 'lib/mailer/Mailer.php';
+	    	$segment = Model::load( 'Segments' )->firstById ( MeuMobi::segment () );
+	    	$mailer = new Mailer(array(
+	    	        'from' => $segment->email, 
+	    	        'to' => $to,
+	    	        'subject' => $title, 
+	    	        'views' => array('text/html' => $template ), 
+	    	        'layout' => 'mail', 
+	    	        'data' => $data,
+	    	        ));
+	    	$mailer->send ();
+	    }
+	}
+	
+	protected function authenticate($created) 
+	{
 		if ($created || Auth::loggedIn ()) {
 			Auth::login ( $this );
 		}
 	}
 
-	protected function joinName($data) {
+	protected function joinName($data) 
+	{
 		if (array_key_exists ( 'firstname', $data ) && array_key_exists ( 'lastname', $data )) {
 			$data ['name'] = $data ['firstname'] . ',' . $data ['lastname'];
 		}
