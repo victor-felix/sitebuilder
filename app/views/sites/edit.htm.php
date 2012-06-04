1a2d0a73d496a377aa805048f599a23bb51b1224
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-06-04 15:20:20 -0300

    add remove site link

--- app/views/sites/edit.htm.php
+++ app/views/sites/edit.htm.php
@@ -24,6 +24,23 @@
     <?php echo $this->form->submit(s('Save'), array(
         'class' => 'ui-button red larger'
     )) ?>
+    <?php echo $this->html->link($this->html->image('shared/categories/delete.gif') . s('Delete site'), '/sites/remove/' . $site->id, array(
+            'class' => 'ui-button delete has-confirm','data-confirm' => '#delete-confirm'
+    )) ?>
 </fieldset>
 
 <?php echo $this->form->close() ?>
+
+<div id="delete-confirm" class="delete-confirm">
+    <div class="wrapper">
+        <p>
+            <?php echo s('Really want to delete the <strong>%s</strong> site?', e($site->title)) ?>
+            <br />
+            <?php echo s('This will also delete all items and categories') ?>
+        </p>
+        <?php echo $this->html->link(s('Yes, delete'), '/sites/remove/' . $site->id, array(
+            'class' => 'ui-button delete highlight'
+        )) ?>
+        <?php echo $this->html->link(s("No, I don't"), '#', array( 'class' => 'ui-button' )) ?>
+    </div>
+</div>
