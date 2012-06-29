b26654c6ce7cba659734601df52adb51829f50e5
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-06-29 15:28:27 -0300

    change resize

--- config/settings.app.php
+++ config/settings.app.php
@@ -3,7 +3,7 @@
 Config::write('Sites.blacklist', array());
 
 Config::write('SiteLogos.resizes', array('200x200'));
-Config::write('SitePhotos.resizes', array('80x80#', '139x139#', '320x200#'));
+Config::write('SitePhotos.resizes', array('80x80#', '139x139#', '320x200'));
 Config::write('BusinessItems.resizes', array('80x60#', '85x85#', '80x80#', '30x30#', '139x139#', '173x154#'));
 
 Config::write('Segments', array(
