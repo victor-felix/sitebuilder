0101c0e9b998c61869128ef949bfc1ae4d394d77
Victor Dias <victor.dias@ks359225.kimsufi.com>
2012-10-03 04:17:29 +0200

    add integration config env.

---
+++ config/environments/integration.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('error_reporting', -1);
+ini_set('display_errors', 'On');
+
+Config::write('Mail.preventSending', true);
+Config::write('Debug.showErrors', true);
+
+Config::write('Api.ignoreAuth', true);
+
+Config::write('Themes.url', 'http://meu-cloud-db.int-meumobi.com/configs.json');
+Config::write('SiteManager.url', 'http://meu-site-manager.meumobilesite.com');
+Config::write('TemplateEngine.url', 'http://meu-template-engine.int-meumobi.com');
