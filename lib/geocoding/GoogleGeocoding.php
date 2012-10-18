09674ed601e6b012ca9ed5a1f6ec93120496a992
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-10-18 11:41:08 -0300

    add multigeeocode urls support

--- lib/geocoding/GoogleGeocoding.php
+++ lib/geocoding/GoogleGeocoding.php
@@ -5,7 +5,8 @@ class GoogleGeocoding {
     const FORMAT = 'json';
     const LANGUAGE = 'pt-BR';
     const SENSOR = 'false';
-
+	static $CURRENT_URL = 0;
+    
     public static function geocode($address,$region = self::REGION) {
         $query = http_build_query(array(
             'region' => $region,
@@ -13,14 +14,42 @@ class GoogleGeocoding {
             'sensor' => self::SENSOR,
             'address' => $address
         ));
-        $url = 'http://maps.googleapis.com/maps/api/geocode/' . self::FORMAT . '?' . $query;
+        $url = self::geocodeUrl() . '/maps/api/geocode/' . self::FORMAT . '?' . $query;
 
         $log = \KLogger::instance(\Filesystem::path('log'));
         $log->logInfo('Geocode Request: %s', $url);
-
-        return self::request($url);
+		
+        $request = self::request($url);
+        if ($request->status == 'OVER_QUERY_LIMIT' && self::geocodeUrl(true)) {
+        	$request = self::geocode($address, $region);
+        }
+        return $request;
     }
-
+	
+    protected static function geocodeUrl($goNext = null)
+    {
+    	$urls = Config::read('Geocode.urls');
+    	
+    	if ($urls) {
+    		$urls = (array) $urls;
+    		//set new url
+    		if ($goNext) { 
+    			$next = self::$CURRENT_URL + 1;
+    			if (isset($urls[$next])) {
+    				self::$CURRENT_URL = $next;
+    			} else {
+    				return false;
+    			}
+    		}
+    		
+    		$url = $urls[self::$CURRENT_URL];
+    	} else {
+    		$url = 'http://maps.googleapis.com';
+    	}
+    	
+    	return $url;
+    }
+    
     protected static function request($url) {
         $remote = curl_init($url);
         curl_setopt($remote, CURLOPT_HEADER, 0);
