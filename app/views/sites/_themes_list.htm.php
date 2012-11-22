36ae740350a190d6262e7a49fe360c2c796d4104
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-11-22 16:52:42 -0300

    uxreview: add themes list template

---
+++ app/views/sites/_themes_list.htm.php
@@ -0,0 +1,56 @@
+<?php 
+	$currentTheme = $site->theme ? $site->theme : $themes[0]->_id;
+	$currentSkin = $site->skin ? $site->skin : key($themes[0]->colors);
+?>
+<fieldset>
+	<h2><?php echo s('themes') ?></h2>
+	<div class="field-group">
+		<div class="form-grid-460 first">
+			<h3><?php echo s('pick up a theme')?></h3>
+			<p style="margin: 0 0 15px 0"><?php echo s('give your mobile a custom look with one of the themes bellow. choose the one that fits you best.') ?></p>
+			<div class="theme-picker">
+				<h3><?php echo s('Select a theme <em>(more themes very soon)</em>') ?></h3>
+				<ul>
+					<?php foreach($themes as $theme): ?>
+						<li class="<?php if($theme->_id == $currentTheme) echo 'selected'?>" data-theme="<?php echo $theme->_id ?>">
+							<p class="thumbs">
+							<?php foreach ($theme->thumbnails as $thumbnail): ?>
+								<?php echo $this->html->image(Themes::thumbPath($thumbnail)) ?>
+							<?php endforeach ?>
+							</p>
+							<span><?php echo $theme->name ?></span>
+							
+							<ul class="skin-picker">
+								<?php 
+								$skins = array_keys((array) $theme->colors);
+								$currentThemeSkin = $currentTheme == $theme->_id && in_array($currentSkin, $skins)
+													? $currentSkin
+													: reset($skins);
+								?>
+								
+								<?php foreach($skins as $skin): ?>
+								<li class="<?php if($skin == $currentThemeSkin) echo 'selected'?>" data-skin="<?php echo $skin ?>">
+									<span style="background-color:#<?php echo $skin ?>"></span>
+								</li>
+								<?php endforeach ?>
+							</ul>
+						</li>
+					<?php endforeach ?>
+				</ul>
+				<div class="clear"></div>
+			</div>
+			
+			<?php echo $this->form->input('theme', array(
+					'type' => 'hidden',
+					'value' => $currentTheme,
+				'id' => 'theme'
+			)) ?>
+			
+			<?php echo $this->form->input('skin', array(
+					'type' => 'hidden',
+					'value' => $currentSkin,
+				'id' => 'skin'
+			)) ?>
+		</div>
+	</div>
+</fieldset>
