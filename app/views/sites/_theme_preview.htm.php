ee7b0ba36f26bb1f36104c01f75f68fab5e8bcea
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-11-28 10:12:04 -0300

    remove test url

--- app/views/sites/_theme_preview.htm.php
+++ app/views/sites/_theme_preview.htm.php
@@ -1,7 +1,6 @@
 <div class="theme-preview">
 	<div class="wrapp">
 		<?php $url = "http://{$site->domain}" ?>
-		<?php $url = "http://m.santacasajf.org.br" ?>
 		<div class="load"></div>
 		<iframe 
 			id="theme-frame" 
