023e70f365ab6d2fb5bfde23c2ad524d370a0c63
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-12-04 09:02:51 -0300

    update routes

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,8 @@ Mapper::prefix('api');
 
 Mapper::connect("/settings", "/sites/edit");
 Mapper::connect("/settings/account", "/users/edit");
+Mapper::connect("/settings/general", "/sites/general");
+Mapper::connect("/settings/custom_domain", "/sites/custom_domain");
 Mapper::connect("/settings/customize", "/sites/customize_edit");
 Mapper::connect("/register", "/users/register");
 Mapper::connect("/login", "/users/login");
