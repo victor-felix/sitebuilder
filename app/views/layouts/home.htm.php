eef092c6231565dddbbfe4d8e7d3dc38b96fd949
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-12-11 08:21:28 -0300

    uxreview implement home

--- app/views/layouts/home.htm.php
+++ app/views/layouts/home.htm.php
@@ -1,85 +1,65 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php echo $this->html->charset() ?>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
         <title><?php echo s('home/index.pagetitle') ?></title>
         <link rel="shortcut icon" href="<?php echo Mapper::url("/images/layout/favicon.png") ?>" type="image/png" />
-        <?php echo $this->html->stylesheet('shared/home', 'shared/uikit', 'segment') ?>
+        <?php echo $this->html->stylesheet('shared/bootstrap.min', 'shared/home') ?>
     </head>
 
     <body>
-        <div id="header">
-            <div class="logo">
-                <?php echo $this->html->imagelink('layout/logo.png', '/', array(
-                    'alt' => $this->controller->getSegment()->title
-                ), array(
-                    'class' => 'logo'
-                )) ?>
-            </div>
+    	<div class="header container">
+    		<h1 class="pull-left">
+    			<img alt="MeuMobi" src="/images/shared/home/logo.png" />
+    			<span class="border"></span>
+    		</h1>
+    		<div class="pull-right">
+	    		<p class="call pull-left">
+	    			<span>need convincing? call us</span>
+	    			+55 21 4042.7270
+	    		</p>
+	    		<p class="login pull-left">
+		    		<a class="active" href="#">sign up now</a>
+		    		or
+		    		<a href="#">sign in</a>
+	    		</p>
+    		</div>
+    	</div>
+    	<?php echo $this->contentForLayout ?>
+		<div class="footer">
+			<div class="container">
+				<div class="links" >
+					<a class="logo" href="#">Meumobi</a>
+					<a href="#">About Us</a>
+					<a href="#">Our Blog</a>
+					<a href="#">Support</a>
+					<p class="copy"><?php echo s('&copy;2011 MeuMobi. All rights reserved') ?></p>
+				</div>
+				<div class="contact">
+					<div>
+					<p class="upper">Contact Us</p>
+					<p>
+						email
+						<span>contact@meumobi.com</span>
+					</p>
+					
+					<p>phone
+						<span>+55 21 4042.7270</span>	
+					</p>
+					</div>
+				</div>
+				<div class="social">
+					<span class="upper">Find us on</span>
+					<a class="face" href="#">facebook</a>
+					<a class="twitter" href="#">twitter</a>
+				</div>
+			</div>
+		</div>
+        <?php //echo $this->element('layouts/footer') ?>
 
-            <p class="login">
-                <?php echo $this->html->link(s('Log in'), '/login') ?>
-                <?php if(Users::signupIsEnabled()): ?>
-                 <?php echo s('or') ?>
-                 <?php echo $this->html->link(s('Sign Up'), '/register') ?>
-                <?php endif; ?>
-            </p>
-
-            <div class="get-started">
-                <h2><?php echo s('home/index.title') ?></h2>
-                <p class="subtitle"><?php echo s('home/index.subtitle') ?></p>
-                <?php echo $this->html->link(s('create your mobi now!'), '/register') ?>
-            </div>
-
-            <div id="slideshow">
-                <?php echo $this->html->image('home/slides/iphone.png'); ?>
-                <?php echo $this->html->image('home/slides/blackberry.png'); ?>
-                <?php echo $this->html->image('home/slides/android.png'); ?>
-            </div>
-
-            <div class="clear"></div>
-            <div id="login-window">
-                <p><?php echo $this->html->link(s('Log in'), '/login') ?></p>
-                <?php echo $this->form->create('/users/login') ?>
-                    <?php echo $this->form->input('email', array(
-                        'label' => s('E-Mail'),
-                        'class' => 'ui-text'
-                    )) ?>
-                    
-                    <?php echo $this->form->input('password', array(
-                        'label' => s('Password'),
-                        'class' => 'ui-text'
-                    )) ?>
-                    
-                    <?php echo $this->form->input('remember', array(
-                        'label' => false,
-                        'type' => 'checkbox'
-                    )) ?>
-                    
-                    <label for="FormRemember" class="checkbox"><?php echo s('Remember me') ?></label>
-                    
-                    <?php echo $this->html->link(s('Forgot password?'), '/users/forgot_password', array(
-                        'class' => 'forgot-password'
-                    )) ?>
-                    
-                    <?php echo $this->form->submit(s('Log in'), array(
-                        'class' => 'ui-button red'
-                    ))?>
-                    
-                    <?php echo $this->html->link(s('cancel'), '#', array(
-                        'class' => 'cancel'
-                    )) ?>
-                <?php echo $this->form->close() ?>
-            </div>
-        </div>
-
-        <div id="content">
-            <?php echo $this->contentForLayout ?>
-        </div>
-
-        <?php echo $this->element('layouts/footer') ?>
-
-        <?php echo $this->html->script('shared/jquery', 'shared/jquery.cycle', 'shared/home') ?>
+        <?php echo $this->html->script() ?>
         <?php echo $this->html->scriptsForLayout ?>
     </body>
 </html>
