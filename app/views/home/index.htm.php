01ec13a58b1517650bd67a1b234de7efe9beaa25
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2012-12-11 10:50:39 -0300

    uxreview add translated tokens

--- app/views/home/index.htm.php
+++ app/views/home/index.htm.php
@@ -3,10 +3,10 @@
 			<div class="container">
 				<div class="hero-unit clearfix">
 					<div class="block pull-left">
-						<h3>Create a mobile website for your business</h3>
-						<p>Works with over 6000 cellphones, smartphones and tablets.</p>
-						<a class="btn big" href="#">start your free trial</a>
-						<p class="trial"><small><b>NO</b> Credit card required 14-day<br> fully functional trial</small></p>
+						<h3><?php echo s('Create a mobile website for your business')?></h3>
+						<p><?php echo s('Works with over 6000 cellphones, smartphones and tablets.')?></p>
+						<a class="btn big" href="#"><?php echo s('start your free trial')?></a>
+						<p class="trial"><small><b><?php echo s('NO')?></b> <?php echo s('Credit card required 14-day')?><br> <?php echo s('fully functional trial')?></small></p>
 				  	</div>
 				  	<div class="pull-right">
 					  <div class="slider ">
@@ -23,35 +23,35 @@
 		<div class="presentation outer wrapp">
 			<div class="container">
 				<div class="row">
-					<h3 class="title span4">Everything your company needs to a great mobile presence.</h3>
+					<h3 class="title span4"><?php echo s('Everything your company needs to a great mobile presence.')?></h3>
 				</div>
 				<ul class="thumbnails">
 				  <li class="span3">
 				    <div class="thumbnail">
 				      <img src="/images/shared/home/icon-middle-04.png" alt="">
-				      <h3>Easy to use control panel</h3>
-				      <p>Quickly manage the content of your mobile website in a full-featured, easy to use, control panel.</p>
+				      <h3><?php echo s('Easy to use control panel')?></h3>
+				      <p><?php echo s('Quickly manage the content of your mobile website in a full-featured, easy to use, control panel.')?></p>
 				    </div>
 				  </li>
 				  <li class="span3">
 				    <div class="thumbnail">
 				      <p><img src="/images/shared/home/icon-middle-02.png" alt=""></p>
-				      <h3>Professionally designed themes</h3>
-				      <p>Get your the right look with premium themes that fit your business segment, whether it's an restaurant or an hospital.</p>
+				      <h3><?php echo s('Professionally designed themes')?></h3>
+				      <p><?php echo s('Get your the right look with premium themes that fit your business segment, whether it´s an restaurant or an hospital.')?></p>
 				    </div>
 				  </li>
 				  <li class="span3">
 				    <div class="thumbnail">
 				      <img src="/images/shared/home/icon-middle-03.png" alt="">
-				      <h3>Add your products, services & menus</h3>
-				      <p>Your mobile website has space for your restaurant menu, your products, services, photos, and more.</p>
+				      <h3><?php echo s('Add your products, services & menus')?></h3>
+				      <p><?php echo s('Your mobile website has space for your restaurant menu, your products, services, photos, and more.')?></p>
 				    </div>
 				  </li>
 				  <li class="span3">
 				    <div class="thumbnail">
 				      <img src="/images/shared/home/icon-middle-01.png" alt="">
-				      <h3>Compatible with over 6000 devices</h3>
-				      <p>From the cutting edge smartphones to the oldest and cheapest cell phones, your mobile website is for everyone.</p>
+				      <h3><?php echo s('Compatible with over 6000 devices')?></h3>
+				      <p><?php echo s('From the cutting edge smartphones to the oldest and cheapest cell phones, your mobile website is for everyone.')?></p>
 				    </div>
 				  </li>
 				</ul>
@@ -61,19 +61,19 @@
 			<div class="container">
 				<div class="row">
 					<div class="span4">
-						<h3 class="title">Get the most out of the mobile web with these features.</h3>
+						<h3 class="title"><?php echo s('Get the most out of the mobile web with these features.')?></h3>
 						<div class="call block">
 							<span class="icon"><img alt="" src="/images/shared/home/fone.png"></span>
-							<h2>Click-to-call</h2>
+							<h2><?php echo s('Click-to-call')?></h2>
 							<p>
-								Get your the right look with premium themes that fit your business segment, whether it's an restaurant or an hospital.
+								<?php echo s('Get your the right look with premium themes that fit your business segment, whether it´s an restaurant or an hospital.')?>
 							</p>
 						</div>
 						<div class="map block">
 							<span class="icon"><img alt="" src="/images/shared/home/marker.png"></span>
-							<h2>Map & direction</h2>
+							<h2><?php echo s('Map & direction')?></h2>
 							<p>
-								Get your the right look with premium themes that fit your business segment, whether it's an restaurant or an hospital.
+								<?php echo s('Get your the right look with premium themes that fit your business segment, whether it´s an restaurant or an hospital.')?>
 							</p>
 						</div>
 					</div>
@@ -82,9 +82,9 @@
 							<div class="span4">
 								<div class="store-locator block">
 									<span class="icon"><img alt="" src="/images/shared/home/lupa.png"></span>
-									<h2>Store locator</h2>
+									<h2><?php echo s('Store locator')?></h2>
 									<p>
-										A map interface with geolocated address for business with multiple locations.
+										<?php echo s('A map interface with geolocated address for business with multiple locations.')?>
 									</p>
 								</div>
 								<div class="phone">
@@ -94,9 +94,9 @@
 							<div class="span4">
 								<div class="rss block">
 									<span class="icon"><img alt="" src="/images/shared/home/rss.png"></span>
-									<h2>RSS feed</h2>
+									<h2><?php echo s('RSS feed')?></h2>
 									<p>
-										Import any content automatically from a RSS feed. Great for online publishers.
+										<?php echo s('Import any content automatically from a RSS feed. Great for online publishers.')?>
 									</p>
 								</div>
 								<div class="phone">
@@ -110,42 +110,42 @@
 		</div>
 		<div class="prices outer wrapp">
 			<div class="container">
-				<h3>Our plans and prices.</h3>
+				<h3><?php echo s('Our plans and prices.')?></h3>
 				<div class="row">
 					<div class="span6">
-						<h2><span>Basic</span>$9/mo</h2>
-						<p>for personal, non-professional and small businesses</p>
+						<h2><span><?php echo s('Basic')?></span><?php echo s('$9/mo')?></h2>
+						<p><?php echo s('for personal, non-professional and small businesses')?></p>
 						
 						<ul>
-							<li><span class="icon"></span>up to <b>10 products and services</b></li>
-							<li><span class="icon"></span><b>free</b> community support</li>
-							<li><span class="icon"></span><b>basic</b> themes</li>
-							<li><span class="icon"></span><b>click-to-call</b>, maps & directions</li>
-							<li class="uncheck"><span class="icon"></span>more features? checkout Pro &#62;</li>
+							<li><span class="icon"></span><?php echo s('up to <b>10 products and services</b>')?></li>
+							<li><span class="icon"></span><?php echo s('<b>free</b> community support')?></li>
+							<li><span class="icon"></span><?php echo s('<b>basic</b> themes')?></li>
+							<li><span class="icon"></span><?php echo s('<b>click-to-call</b>, maps & directions')?></li>
+							<li class="uncheck"><span class="icon"></span><?php echo s('more features? checkout Pro')?> &#62;</li>
 						</ul>
 					</div>
 					<div class="span6">
-						<h2><span>Pro</span>$19/mo</h2>
-						<p>for medium and large businesses and professionals</p>
+						<h2><span><?php echo s('Pro')?></span><?php echo s('$19/mo')?></h2>
+						<p><?php echo s('for medium and large businesses and professionals')?></p>
 						
 						<ul>
-							<li><span class="icon"></span>up to <b>100 products and services</b></li>
-							<li><span class="icon"></span>full online support by <b>chat, phone and email</b></li>
-							<li><span class="icon"></span><b>premium</b> themes</li>
-							<li><span class="icon"></span><b>store locator</b>, click-to-call, maps & directions</li>
-							<li><span class="icon"></span><b>extensions</b>, web analytics, and more!</li>
+							<li><span class="icon"></span<?php echo s('>up to <b>100 products and services</b>')?></li>
+							<li><span class="icon"></span><?php echo s('full online support by <b>chat, phone and email')?></b></li>
+							<li><span class="icon"></span><?php echo s('<b>premium</b> themes')?></li>
+							<li><span class="icon"></span><?php echo s('<b>store locator</b>, click-to-call, maps & directions')?></li>
+							<li><span class="icon"></span><?php echo s('<b>extensions</b>, web analytics, and more!')?></li>
 						</ul>
 					</div>
 				</div>
 				
 				<p class="action">
-					<a class="btn big" href="#">start your free trial now!</a>
+					<a class="btn big" href="#"><?php echo s('start your free trial now!')?></a>
 					<br>
 					<span class="trial">
 						<small>
-						NO <b>credit card required!</b>
+						<?php echo s('NO')?> <b><?php echo s('credit card required!')?></b>
 						<br>
-						you'll be able to choose a plan after the trial.
+						<?php echo s('you´ll be able to choose a plan after the trial.')?>
 						</small>
 					</span>
 				</p>
@@ -156,13 +156,13 @@
 				<div class="row">
 					<div class=" span4 ">
 						<div class="insights">
-							<h3><b>get mobile.</b><br> a few insights on the mobile market.</h3>
+							<h3><?php echo s('<b>get mobile.</b><br> a few insights on the mobile market.')?></h3>
 							
 							<div class="slider">
 								<div style="width: 255px;">
 									<p>
-										<b>40% percent</b> of all mobile web searches pertain to the searcher's local environment, 
-										meaning customers are looking for goods and services they need while on the go
+										<?php echo s('<b>40% percent</b> of all mobile web searches pertain to the searcher´s local environment, 
+										meaning customers are looking for goods and services they need while on the go')?>
 									</p>
 									<p class="author">Google Research, 2012</p>
 								</div>
@@ -187,23 +187,23 @@
 						</div>
 					</div>
 					<div class="about span8">
-						<h3>what our clients, partners and press say about us</h3>
+						<h3><?php echo s('what our clients, partners and press say about us')?></h3>
 						<div class="row">
 							<div class="span4">
 								<div class="quote">
 									<p>
-										MeuMobi provides a very innovative mobile marketing solution to businesses, brands and media companies. They're awesome.
+										<?php echo s('MeuMobi provides a very innovative mobile marketing solution to businesses, brands and media companies. They´re awesome.')?>
 									</p>
-									<p class="author">Google Research, 2012</p>
+									<p class="author"><?php echo s('Google Research, 2012')?></p>
 								</div>
 							</div>
 							
 							<div class="span4">
 								<div class="quote">
 									<p>
-										This company is great! Their service is awesome, being their main advantage the simplicity and beauty of the website.
+										<?php echo s('This company is great! Their service is awesome, being their main advantage the simplicity and beauty of the website.')?>
 									</p>
-									<p class="author">Google Research, 2012</p>
+									<p class="author"><?php echo s('Google Research, 2012')?></p>
 								</div>
 							</div>
 						</div>
@@ -213,9 +213,9 @@
 		</div>
 		<div class="container ">
 			<div class="custom-solution">
-				<h3>need custom solutions?</h3>
+				<h3><?php echo s('need custom solutions?')?></h3>
 				<p>
-				Create custom layout, integration with any CMS, open API, business with multiple location, any custom solutions. Great for agencies, resellers, and big companies.
+				<?php echo s('Create custom layout, integration with any CMS, open API, business with multiple location, any custom solutions. Great for agencies, resellers, and big companies.')?>
 				</p>
 			</div>
 		</div>
