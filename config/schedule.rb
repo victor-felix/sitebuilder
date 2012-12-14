aa81b99381fbd8dc3bbbffc3dfdaffb943936f57
juliogreff <juliogreff@gmail.com>
2012-12-14 12:32:56 -0200

    Added schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,1 @@
-set :output, 'log/whenever.log'
-
-every 10.minutes do
-  command 'php meu-site-builder/script/run_import.php'
-end
-
-every 1.hour do
-  command 'php meu-site-builder/script/run_geocode.php'
-end
+../meu-site-builder/schedule.rb
