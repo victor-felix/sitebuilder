ffd2edc786138cf366de2761b4f99721a3c7a18d
juliogreff <juliogreff@gmail.com>
2012-12-28 14:40:13 -0200

    Fixes schedule.rb paths

--- schedule.rb
+++ schedule.rb
@@ -1,13 +1,13 @@
-set :output, 'log/whenever.log'
+set :output, File.expand_path('meu-site-builder/log/whenever.log')
 
 every 10.minutes do
-  command 'php meu-site-builder/script/update_feeds.php'
+  command "php #{File.expand_path 'meu-site-builder/script/update_feeds.php'}"
 end
 
 every 10.minutes do
-  command 'php meu-site-builder/script/run_import.php'
+  command "php #{File.expand_path 'meu-site-builder/script/run_import.php'}"
 end
 
 every 1.hour do
-  command 'php meu-site-builder/script/run_geocode.php'
+  command "php #{File.expand_path 'meu-site-builder/script/run_geocode.php'}"
 end
