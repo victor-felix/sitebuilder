435a721e77a726d624f19b01aa0707b7a9feac72
juliogreff <juliogreff@gmail.com>
2013-01-22 19:20:10 -0200

    Set meu-cloud-db to elefante

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,6 +5,6 @@ ini_set('display_errors', 'Off');
 
 Config::write('Yaml.cache', true);
 
-Config::write('Themes.url', 'http://meu-cloud-db.int-meumobilesite.com/configs.json');
+Config::write('Themes.url', 'http://meu-cloud-db.meumobi.com/configs.json');
 Config::write('SiteManager.url', 'http://meu-site-manager.meumobi.com');
 Config::write('TemplateEngine.url', 'http://meu-template-engine.meumobi.com');
