d198428d46d43cdea1268c4411851705fabd7e85
juliogreff <juliogreff@gmail.com>
2013-01-22 19:04:52 -0200

    Adds enterprise segment

---
+++ segments/enterprise/config.php
@@ -0,0 +1,13 @@
+<?php
+
+Config::write('Segment', array(
+  'id' => 'enterprise',
+  'title' => 'MeuMobi Enterprise',
+  'items' => array('articles', 'business'),
+  'extensions' => array('store-locator', 'rss'),
+  'root' => 'Index',
+  'email' => array('no-reply@meumobi.com' => 'MeuMobi Enterprise'),
+  'hideCategories' => false,
+  'enableSignup' => false,
+  'primaryColor' => '#000'
+));
