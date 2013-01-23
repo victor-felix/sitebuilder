dd7befcbdbf69151225cb7258864037a469c5b5c
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-01-23 09:40:15 -0300

    change meumobi segment colors

--- segments/meumobi/config.php
+++ segments/meumobi/config.php
@@ -4,11 +4,11 @@ Config::write('Segment', array(
   'id' => 'meumobi',
   'title' => 'MeuMobi',
   'items' => array('articles', 'business', 'users'),
-  'extensions' => array(''),
+  'extensions' => array('rss','store-locator'),
   'root' => 'Index',
   'email' => array('no-reply@meumobi.com' => 'MeuMobi'),
   'hideCategories' => false,
-  'enableSignup' => false,
+  'enableSignup' => 1,
   'fullOptions' => false,
   'primaryColor' => '#ccc'
 ));
