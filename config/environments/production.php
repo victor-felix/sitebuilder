5f82f5491cae2419eeedb1249931772624bdf88c
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-01-29 15:17:59 -0300

    escape pt-BR translations and disable api auth in production

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 'Off');
 
 Config::write('Yaml.cache', true);
 
+Config::write('Api.ignoreAuth', true);
+
 Config::write('Themes.url', 'http://meu-cloud-db.meumobi.com/configs.json');
 Config::write('SiteManager.url', 'http://meu-site-manager.meumobi.com');
 Config::write('TemplateEngine.url', 'http://meu-template-engine.meumobi.com');
