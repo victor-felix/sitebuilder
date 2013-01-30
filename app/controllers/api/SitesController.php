f89da51b198e1a63ddaa543c5bae72e7bc64a68a
juliogreff <juliogreff@gmail.com>
2013-01-30 20:29:12 -0200

    Removes need for authentication for site info

--- app/controllers/api/SitesController.php
+++ app/controllers/api/SitesController.php
@@ -5,22 +5,21 @@ namespace app\controllers\api;
 use Model;
 
 class SitesController extends ApiController {
-    public function show() {
-		$this->requireUserAuth();
-        $slug = $this->param('slug', $this->param('id'));
-        $site = Model::load('Sites')->firstByDomain($slug);
-        if($slug) {
-            $site = Model::load('Sites')->firstByDomain($slug);
-        }
-        else {
-            $site = $this->site;
-        }
+	public function show() {
+		$slug = $this->param('slug', $this->param('id'));
+		$site = Model::load('Sites')->firstByDomain($slug);
+		if($slug) {
+			$site = Model::load('Sites')->firstByDomain($slug);
+		}
+		else {
+			$site = $this->site;
+		}
 
-        $etag = $this->etag($site);
-        $self = $this;
+		$etag = $this->etag($site);
+		$self = $this;
 
-        return $this->whenStale($etag, function() use($site, $self) {
-            return $self->toJSON($site);
-        });
-    }
+		return $this->whenStale($etag, function() use($site, $self) {
+			return $self->toJSON($site);
+		});
+	}
 }
