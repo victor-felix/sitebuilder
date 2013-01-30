77ee270b75936401f3c9a2cdcbce73b9788ec8ff
juliogreff <juliogreff@gmail.com>
2013-01-30 20:33:42 -0200

    Fixes wrong log format

--- script/update_feeds.php
+++ script/update_feeds.php
@@ -4,6 +4,8 @@ require dirname(__DIR__) . '/config/bootstrap.php';
 require 'config/settings.php';
 require 'config/connections.php';
 
+echo date('Y-m-d H:i:s') . ': Updating feeds...' . PHP_EOL;
+
 $categories = Model::load('Categories')->all(array(
     'conditions' => array(
         'feed_url IS NOT NULL AND feed_url != ""'
@@ -13,5 +15,3 @@ $categories = Model::load('Categories')->all(array(
 foreach($categories as $category) {
     $category->updateArticles();
 }
-
-echo 'Updated feeds at ' . date('Y-m-d H:i:s');
