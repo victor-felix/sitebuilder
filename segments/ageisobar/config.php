504fc53f0464882d3a0fa859333a8971ea50b393
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-02-04 18:24:32 -0300

    add ageisobar segment

---
+++ segments/ageisobar/config.php
@@ -0,0 +1,13 @@
+<?php
+
+Config::write('Segment', array(
+  'id' => 'ageisobar',
+  'title' => 'Ageisobar',
+  'items' => array('articles', 'business'),
+  'extensions' => array('store-locator', 'rss'),
+  'root' => 'Index',
+  'email' => array('no-reply@ageisobar.com.br' => 'Ageisobar'),
+  'hideCategories' => false,
+  'enableSignup' => 1,
+  'primaryColor' => ''
+));
