4e9f822c16b971f6da1a21736c67f461709c45ab
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-02-06 18:10:31 -0300

    add fbiz segment

---
+++ segments/fbiz/config.php
@@ -0,0 +1,12 @@
+<?php
+
+Config::write('Segment', array(
+  'id' => 'fbiz',
+  'title' => 'f.biz',
+  'items' => array('articles', 'business'),
+  'extensions' => array('store-locator', 'rss'),
+  'root' => 'Index',
+  'email' => array('no-reply@fbiz.com.br' => 'f.biz'),
+  'hideCategories' => false,
+  'enableSignup' => 1,
+));
