b43599ece89ba865036e91db7c4124a20c2c380a
juliogreff <juliogreff@gmail.com>
2013-02-21 11:16:37 -0300

    Fixes warning on theme preview

--- app/views/sites/_theme_preview.htm.php
+++ app/views/sites/_theme_preview.htm.php
@@ -4,6 +4,6 @@
 		<iframe id="theme-frame"
 			src=""
 			data-url="<?php if ($site->id) echo "http://{$site->domain}"; else echo MeuMobi::currentSegment()->sitePreviewUrl() ?>"
-			<?php if ($autoload) echo 'data-autoload="1"' ?>></iframe>
+			<?php if (isset($autoload) && $autoload) echo 'data-autoload="1"' ?>></iframe>
 	</div>
 </div>
