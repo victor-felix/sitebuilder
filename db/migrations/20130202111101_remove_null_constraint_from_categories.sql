62fe7c7cad7c86881b4bc27f9aea5bc6bf7dcafc
juliogreff <juliogreff@gmail.com>
2013-03-01 10:01:09 -0300

    Fixes wrong migration promoting subcategories to categories when root was removed

--- db/migrations/20130202111101_remove_null_constraint_from_categories.sql
+++ db/migrations/20130202111101_remove_null_constraint_from_categories.sql
@@ -1,3 +1,3 @@
 ALTER TABLE categories MODIFY COLUMN parent_id INT(12) DEFAULT NULL;
-UPDATE categories a INNER JOIN categories b ON a.parent_id = b.id SET a.parent_id = NULL;
+UPDATE categories a INNER JOIN categories b ON a.parent_id = b.id SET a.parent_id = NULL WHERE b.parent_id = 0;
 DELETE FROM categories WHERE parent_id = 0;
