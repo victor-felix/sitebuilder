bb62ff8eee92a0b0809177879ffdf20ec66c5b4d
juliogreff <juliogreff@gmail.com>
2013-03-05 10:50:08 -0300

    Updates sitebuilder

--- segments/meumobi/config.php
+++ segments/meumobi/config.php
@@ -3,11 +3,10 @@
 Config::write('Segment', array(
   'id' => 'meumobi',
   'title' => 'MeuMobi',
+  'root' => 'Index',
   'items' => array('articles', 'business', 'users'),
   'extensions' => array('rss','store-locator'),
   'email' => array('no-reply@meumobi.com' => 'MeuMobi'),
-  'hideCategories' => true,
-  'enableSignup' => 1,
   'hideCategories' => true,
   'enableSignup' => true,
   'fullOptions' => false,
