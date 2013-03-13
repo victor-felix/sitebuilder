a8925eb832ebfed5aad18830046a0d4f65d085cc
juliogreff <juliogreff@gmail.com>
2013-03-13 13:54:03 -0300

    Cleans up view code

--- app/views/categories/_item.htm.php
+++ app/views/categories/_item.htm.php
@@ -1,59 +1,62 @@
 <li class="level-<?php echo $level ?>" data-catid="<?php echo $category->id ?>" data-parentid="<?php echo $category->parent_id ?>">
-    <?php if($level == 1): ?>
-        <?php echo $this->html->link('','/categories/add/' . $category->id, array(
-            'class' => 'ui-button ui-button-add highlight push-scene'
-        )) ?>
-    <?php endif ?>
+	<?php if($level == 1): ?>
+		<?php echo $this->html->link('', '/categories/add/' . $category->id, array(
+			'class' => 'ui-button ui-button-add highlight push-scene'
+		)) ?>
+	<?php endif ?>
 
-    <span class="title edit-in-place" data-saveurl="/categories/edit/<?php echo $category->id ?>" title="<?php echo s('click to edit') ?>">
-        <?php echo e($category->title) ?>
-    </span>
+	<span class="title edit-in-place" data-saveurl="/categories/edit/<?php echo $category->id ?>" title="<?php echo s('click to edit') ?>">
+		<?php echo e($category->title) ?>
+	</span>
 
-    <div class="controls">
-    	<?php echo $this->html->link(s('down'), '/categories/movedown/' . $category->id, array(
-                'class' => 'ui-button highlight'
-         )) ?>
-    	<?php echo $this->html->link(s('Up'), '/categories/moveup/' . $category->id, array(
-         	'class' => 'ui-button highlight left-join'
-         )) ?>
-        <?php if(!$category->hasFeed()): ?>
-            <?php echo $this->html->link(s('add item'), '/business_items/add/' . $category->id, array(
-                'class' => 'ui-button highlight push-scene'
-            )) ?>
+	<div class="controls">
+		<?php echo $this->html->link(s('down'), '/categories/movedown/' . $category->id, array(
+			'class' => 'ui-button highlight'
+		)) ?>
 
-            <?php echo $this->html->link(s('manage items'), '/business_items/index/' . $category->id, array(
-                'class' => 'ui-button manage push-scene left-join'
-            )) ?>
-        <?php else: ?>
-            <em><?php echo s('auto category')?></em>
-        <?php endif ?>
+		<?php echo $this->html->link(s('up'), '/categories/moveup/' . $category->id, array(
+			'class' => 'ui-button highlight left-join'
+		)) ?>
 
-        <?php echo $this->html->link(s('options'), '/categories/edit/' . $category->id, array(
-            'class' => 'ui-button manage push-scene'
-        )) ?>
-    </div>
+		<?php if(!$category->hasFeed()): ?>
+			<?php echo $this->html->link(s('add item'), '/business_items/add/' . $category->id, array(
+				'class' => 'ui-button highlight push-scene'
+			)) ?>
 
-    <div class="children-count"><?php echo $category->childrenCount() ?></div>
+			<?php echo $this->html->link(s('manage items'), '/business_items/index/' . $category->id, array(
+				'class' => 'ui-button manage push-scene left-join'
+			)) ?>
+		<?php else: ?>
+			<em><?php echo s('auto category')?></em>
+		<?php endif ?>
 
-    <?php if($category->parent_id > 0): ?>
-        <div class="delete-confirm">
-            <div class="wrapper">
-                <p><?php echo s('Really want to delete <strong>%s</strong>?', e($category->title)) ?> <small><?php echo s('All itens and sub-categories related will be deleted') ?></small></p>
-                <?php echo $this->html->link(s('Yes, delete'), '/categories/delete/' . $category->id, array(
-                    'class' => 'ui-button delete highlight'
-                )) ?>
-                <?php echo $this->html->link(s('No, I don\'t'), '#', array( 'class' => 'ui-button' )) ?>
-            </div>
-        </div>
-    <?php endif ?>
+		<?php echo $this->html->link(s('options'), '/categories/edit/' . $category->id, array(
+			'class' => 'ui-button manage push-scene'
+		)) ?>
+	</div>
+
+	<div class="children-count"><?php echo $category->childrenCount() ?></div>
+
+	<div class="delete-confirm">
+		<div class="wrapper">
+			<p>
+				<?php echo s('Really want to delete <strong>%s</strong>?', e($category->title)) ?>
+				<small><?php echo s('All itens and sub-categories related will be deleted') ?></small>
+			</p>
+			<?php echo $this->html->link(s('Yes, delete'), '/categories/delete/' . $category->id, array(
+				'class' => 'ui-button delete highlight'
+			)) ?>
+			<?php echo $this->html->link(s('No, I don\'t'), '#', array( 'class' => 'ui-button' )) ?>
+		</div>
+	</div>
 </li>
 
 <?php if(array_key_exists($category->id, $categories)): ?>
-    <?php foreach($categories[$category->id] as $subcategory): ?>
-        <?php echo $this->element('categories/item', array(
-            'level' => $level + 1,
-            'category' => $subcategory,
-            'categories' => $categories
-        )) ?>
-    <?php endforeach ?>
-<?php endif ?> 
+	<?php foreach($categories[$category->id] as $subcategory): ?>
+		<?php echo $this->element('categories/item', array(
+			'level' => $level + 1,
+			'category' => $subcategory,
+			'categories' => $categories
+		)) ?>
+	<?php endforeach ?>
+<?php endif ?>
--- app/views/sites/_edit_form.htm.php
+++ app/views/sites/_edit_form.htm.php
@@ -1,19 +1,19 @@
 <fieldset>
 	<div class="grid-4 first">
-				<div class="tip">
+		<div class="tip">
 			<h2 class="greater"><?php echo s('Your business description') ?></h2>
 			<p>
 				<?php echo s('We need some basic information about your business to start shaping up your mobile website') ?>
 				<?php if($action == 'register'): ?>
-				<br>
-				<br>
-				<small><?php echo s('you´ll be able to change and add other information later') ?></small>
+					<br />
+					<br />
+					<small><?php echo s('you´ll be able to change and add other information later') ?></small>
 				<?php endif ?>
 			</p>
 		</div>
-		</div>
-		
-		<div class="grid-8"> 
+	</div>
+
+	<div class="grid-8">
 		<div class="field-group">
 			<div class="form-grid-460 first">
 				<?php echo $this->form->input('title', array(
@@ -22,27 +22,29 @@
 					'class' => 'ui-text large greater'
 				)) ?>
 			</div>
-			
+
 			<div class="form-grid-460 first">
-						<?php if($site->logo()): ?>
-								<?php echo $this->html->image($site->logo()->link('200x200'), array(
-										'class' => 'logo'
-								)) ?>
-								<?php echo $this->html->link(s('delete logo'), '/images/delete/' . $site->logo()->id) ?>
-						<?php endif ?>
-						<div class="form-grid-460 first">
-								<span class="optional"><?php echo s('Optional') ?></span>
-								<?php echo $this->form->input('logo', array(
-										'label' => s('Logo'),
-										'type' => 'file',
-										'class' => 'ui-text large'
-								)) ?>
-								<small><?php echo s('To improve appearence of logo on your mobi site, we recommend to use an image on GIF or PNG with transparent background. Max size 50kb') ?></small>
-						</div>
+				<?php if($site->logo()): ?>
+					<?php echo $this->html->image($site->logo()->link('200x200'), array(
+						'class' => 'logo'
+					)) ?>
+					<?php echo $this->html->link(s('delete logo'), '/images/delete/' . $site->logo()->id) ?>
+				<?php endif ?>
+				<div class="form-grid-460 first">
+					<span class="optional"><?php echo s('Optional') ?></span>
+					<?php echo $this->form->input('logo', array(
+						'label' => s('Logo'),
+						'type' => 'file',
+						'class' => 'ui-text large'
+					)) ?>
+					<small><?php echo s('To improve appearence of logo on your mobi site, we recommend to use an image on GIF or PNG with transparent background. Max size 50kb') ?></small>
 				</div>
-			
+			</div>
+
 			<div class="form-grid-460">
-				<span class="optional"><?php echo s('<span id="businessCounter" >500</span> left') ?></span>
+				<span class="optional">
+					<?php echo s('<span id="businessCounter">500</span> left') ?>
+				</span>
 				<?php echo $this->form->input('description', array(
 					'id' => 'businessDescription',
 					'label' => s('Description of business'),
@@ -50,9 +52,11 @@
 					'class' => 'ui-textarea large greater',
 					'maxlength' => 500
 				)) ?>
-				<small><?php echo s('Give the users a brief description of what your business is, what it does, when it was founded, what your main services or products are, and so on.') ?></small>
+				<small>
+					<?php echo s('Give the users a brief description of what your business is, what it does, when it was founded, what your main services or products are, and so on.') ?>
+				</small>
 			</div>
-	
+
 			<div class="form-grid-460 first">
 				<div class="site-mobile-url">
 					<div class="input text">
@@ -70,8 +74,7 @@
 						<div class="clear"></div>
 					</div>
 				</div>
-					<small><?php echo s('If you wish to use a custom domain, you can configure one after this wizard.<br>
-								In the admin panel you will find instructions on haw to proceed') ?></small>
+				<small><?php echo s('If you wish to use a custom domain, you can configure one after this wizard.<br /> In the admin panel you will find instructions on haw to proceed') ?></small>
 			</div>
 		</div>
 	</div>
@@ -79,23 +82,23 @@
 
 <fieldset id="business-address">
 	<div class="grid-4 first">
-				<div class="tip">
+		<div class="tip">
 			<h2><?php echo s('Location') ?></h2>
 			<p>
 				<img class="icon" src="/images/shared/sites/icon-location.png" />
 				<span>
 					<?php echo s('Add a map of your business location to your mobile site') ?>
 				</span>
-				<?php if(1 || $action == 'register'): ?>
-				<br>
-				<small><?php echo s('A map integrated with Google Maps will appear on your mobile site, allowing customers to easily find the address.<br/><br/>If your business has multiple locations, you\'ll be able to add all addresses as items in a category.') ?></small>
-				<?php endif ?>
+				<br />
+				<small>
+					<?php echo s('A map integrated with Google Maps will appear on your mobile site, allowing customers to easily find the address.<br /><br />If your business has multiple locations, you\'ll be able to add all addresses as items in a category.') ?>
+				</small>
 			</p>
 		</div>
-		</div>
-		
-		<div class="grid-8">
-			<div class="field-group">
+	</div>
+
+	<div class="grid-8">
+		<div class="field-group">
 			<div class="form-grid-460 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('street', array(
@@ -104,7 +107,7 @@
 					'class' => 'ui-text large'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('number', array(
@@ -113,7 +116,7 @@
 					'class' => 'ui-text'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('complement', array(
@@ -122,7 +125,7 @@
 					'class' => 'ui-text'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-460 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('zone', array(
@@ -131,7 +134,7 @@
 					'class' => 'ui-text large'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('country_id', array(
@@ -142,7 +145,7 @@
 					'class' => 'ui-select'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('state_id', array(
@@ -153,7 +156,7 @@
 					'empty' => array('')
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('city', array(
@@ -162,7 +165,7 @@
 					'class' => 'ui-text'
 				)) ?>
 			</div>
-	
+
 			<div class="form-grid-220">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('zip', array(
@@ -177,7 +180,7 @@
 
 <fieldset id="business-contact">
 	<div class="grid-4 first">
-				<div class="tip">
+		<div class="tip">
 			<h2><?php echo s('Contact Information') ?></h2>
 			<p>
 				<img class="icon" src="/images/shared/sites/icon-call.png" />
@@ -186,10 +189,10 @@
 				</span>
 			</p>
 		</div>
-		</div>
-		
-		<div class="grid-8"> 
-			<div class="field-group">
+	</div>
+
+	<div class="grid-8">
+		<div class="field-group">
 			<div class="form-grid-220 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('phone', array(
@@ -199,7 +202,7 @@
 				)) ?>
 				<small><?php echo s('Ex.: (00) 0000-0000') ?></small>
 			</div>
-	
+
 			<div class="form-grid-460 first">
 				<span class="optional"><?php echo s('Optional') ?></span>
 				<?php echo $this->form->input('email', array(
@@ -211,123 +214,3 @@
 		</div>
 	</div>
 </fieldset>
-
-<!-- full form start here -->
-<?php if($action != 'register'): ?>
-
-<fieldset>
-	<div class="grid-4 first">
-				<div class="tip">
-			<h2><?php echo s('Open hours') ?></h2>
-		</div>
-		</div>
-		
-		<div class="grid-8">
-		<div class="field-group">
-			<div class="form-grid-460 first">
-				<span class="optional"><?php echo s('Optional') ?></span>
-				<?php echo $this->form->input('timetable', array(
-					'label' => s('Open hours'),
-					'type' => 'textarea',
-					'class' => 'ui-textarea large'
-				)) ?>
-			</div>
-		</div>
-	</div>
-</fieldset>
-
-<fieldset id="business-social">
-	<div class="grid-4 first">
-				<div class="tip">
-			<h2><?php echo s('Your links on web') ?></h2>
-		</div>
-		</div>
-		
-		<div class="grid-8">
-		<div class="field-group">
-			<div class="form-grid-460 first">
-				<span class="optional"><?php echo s('Optional') ?></span>
-				<?php echo $this->form->input('facebook', array(
-					'label' => s('Facebook Page'),
-					'type' => 'text',
-					'class' => 'ui-text large'
-				)) ?>
-				<small><?php echo s('Ex: http://www.facebook.com/username/') ?></small>
-			</div>
-	
-			<div class="form-grid-460 first">
-				<span class="optional"><?php echo s('Optional') ?></span>
-				<?php echo $this->form->input('twitter', array(
-					'label' => s('Twitter Page'),
-					'type' => 'text',
-					'class' => 'ui-text large'
-				)) ?>
-				<small><?php echo s('Ex: http://www.twitter.com/username/') ?></small>
-			</div>
-	
-			<div class="form-grid-460 first">
-				<span class="optional"><?php echo s('Optional') ?></span>
-				<?php echo $this->form->input('website', array(
-					'label' => s('Url of your current website'),
-					'type' => 'text',
-					'class' => 'ui-text large'
-				)) ?>
-				<small><?php echo s('Ex: http://www.yourwebsite.com/') ?></small>
-			</div>
-		</div>
-	</div>
-</fieldset>
-
-<fieldset id="business-photos">
-	<div class="grid-4 first">
-				<div class="tip">
-			<h2><?php echo s('Your business photos') ?></h2>
-		</div>
-		</div>
-		
-		<div class="grid-8">
-		<div class="field-group">
-			<?php if($site->id && $images = $site->photos()): ?>
-				<?php foreach($images as $i => $image): $class = $i % 3 ? '' : 'first' ?>
-					<div class="<?php echo $class ?> picture-upload-container done" style="background-image: url(<?php echo $image->link('139x139') ?>)">
-						<?php echo $this->html->link('', '/images/delete/' . $image->id, array(
-							'class' => 'close'
-						)) ?>
-						<?php echo $this->form->input('image['.$image->id.'][title]', array(
-							'label' => false,
-							'placeholder' => s('edit subtitle'),
-							'class' => 'ui-text large',
-							'value' => $image->title
-						)) ?>
-					</div>
-				<?php endforeach ?>
-			<?php endif ?>
-	
-			<?php $class = (isset($i) ? $i + 1 : 0) % 3 ? '' : 'first' ?>
-			<div class="<?php echo $class ?> picture-upload-container" data-url="/images/add.htm">
-				<input type="hidden" name="image[foreign_key]" value="<?php echo $site->id() ?>" />
-				<input type="hidden" name="image[model]" value="SitePhotos" />
-				<a class="close"></a>
-				<iframe src="about:blank" id="iframe_<?php echo time(); ?>"></iframe>
-				<div class="default">
-			<div class="icon_upload"></div>
-			<?php echo s('add photo'); ?>
-			</div>
-				<div class="wait"><?php echo s('uploading photo...'); ?></div>
-				<?php echo $this->form->input('image[photo]', array(
-					'label' => false,
-					'type' => 'file',
-					'class' => 'ui-text large picture-upload'
-				)) ?>
-				<?php echo $this->form->input('image[ID][title]', array(
-					'label' => false,
-					'placeholder' => s('edit subtitle'),
-					'class' => 'ui-text large'
-				)) ?>
-			</div>
-	
-			<a href="#" class="duplicate-previous">more</a>
-		</div>
-	</div>
-</fieldset>
-<?php endif;?>
