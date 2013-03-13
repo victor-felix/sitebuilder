e9be462ec63d1e8e51217b1dc418ecaee83333f6
juliogreff <juliogreff@gmail.com>
2013-03-13 13:17:26 -0300

    Fixes coding style

--- app/models/categories.php
+++ app/models/categories.php
@@ -2,16 +2,19 @@
 
 require_once 'lib/utils/Works/Import.php';
 require_once 'lib/utils/FileUpload.php';
+require_once 'lib/mailer/Mailer.php';
 
 use app\models\Extensions;
 use app\models\Items;
 use utils\Import;
 
-class Categories extends AppModel {
+class Categories extends AppModel
+{
 	const MAX_IMPORTFILE_SIZE = 300;
 	protected $beforeSave = array('setOrder', 'getItemType');
 	protected $afterSave = array('importItems', 'updateParentTimestamps');
-	protected $beforeDelete = array('deleteChildren', 'updateParentTimestampsWhenDeleted');
+	protected $beforeDelete = array('deleteChildren',
+		'updateParentTimestampsWhenDeleted');
 	protected $defaultScope = array(
 		'order' => '`order` ASC'
 	);
@@ -28,16 +31,18 @@ class Categories extends AppModel {
 		),
 	);
 
-	public function __construct($data = array()) {
+	public function __construct($data = array())
+	{
 		parent::__construct($data);
 
-		if(is_null($this->id) && !isset($this->data['visibility'])) {
+		if (is_null($this->id) && !isset($this->data['visibility'])) {
 			$this->data['visibility'] = true;
 			$this->data['populate'] = 'manual';
 		}
 	}
 
-	public function childrenItems($limit = null) {
+	public function childrenItems($limit = null)
+	{
 		$type = Inflector::underscore($this->type);
 		$classname = '\app\models\items\\' . Inflector::camelize($type);
 
@@ -51,17 +56,19 @@ class Categories extends AppModel {
 		return $this->populate == 'auto';
 	}
 
-	public function childrenCount() {
+	public function childrenCount()
+	{
 		return Items::find('count', array('conditions' => array(
 			'parent_id' => $this->id
 		)));
 	}
 
-	public function breadcrumbs() {
+	public function breadcrumbs()
+	{
 		$parent_id = $this->parent_id;
 		$breadcrumbs = array($this);
 
-		while($parent_id > 0) {
+		while ($parent_id > 0) {
 			$category = $this->firstById($parent_id);
 			$breadcrumbs []= $category;
 			$parent_id = $category->parent_id;
@@ -77,10 +84,11 @@ class Categories extends AppModel {
 		}
 	}
 
-	public function recursiveById($id, $depth) {
+	public function recursiveById($id, $depth)
+	{
 		$results = array($this->firstById($id));
 
-		if($depth > 0) {
+		if ($depth > 0) {
 			$children = $this->recursiveByParentId($id, $depth - 1);
 			$results = array_merge($results, $children);
 		}
@@ -88,11 +96,12 @@ class Categories extends AppModel {
 		return $results;
 	}
 
-	public function recursiveByParentId($parent_id, $depth) {
+	public function recursiveByParentId($parent_id, $depth)
+	{
 		$results = $this->allByParentIdAndVisibility($parent_id, 1);
 
-		if($depth > 0) {
-			foreach($results as $result) {
+		if ($depth > 0) {
+			foreach ($results as $result) {
 				$children = $this->recursiveByParentId($result->id, $depth - 1);
 				$results = array_merge($results, $children);
 			}
@@ -101,7 +110,8 @@ class Categories extends AppModel {
 		return $results;
 	}
 
-	public function toJSON() {
+	public function toJSON()
+	{
 		$data = $this->data;
 		$data['items_count'] = $this->childrenCount();
 		return $data;
@@ -172,12 +182,13 @@ class Categories extends AppModel {
 		return $data;
 	}
 
-	protected function getItemType($data) {
-		if(is_null($this->id)) {
+	protected function getItemType($data)
+	{
+		if (is_null($this->id)) {
 			$site = Model::load('Sites')->firstById($this->site_id);
 			$items = (array) $site->itemTypes();
 
-			if(!array_key_exists('type', $data) || !in_array($data['type'], $items)) {
+			if (!array_key_exists('type', $data) || !in_array($data['type'], $items)) {
 				$data['type'] = $items[0];
 			}
 		}
@@ -185,10 +196,11 @@ class Categories extends AppModel {
 		return $data;
 	}
 
-	protected function importItems($created) {
+	protected function importItems($created)
+	{
 		if (isset($this->data['import']) && is_uploaded_file($this->data['import']['tmp_name'])) {
 			$fileSize = $this->data['import']['size'];
-			if($fileSize && self::MAX_IMPORTFILE_SIZE < ($fileSize / 1024)
+			if ($fileSize && self::MAX_IMPORTFILE_SIZE < ($fileSize / 1024)
 				&& $this->scheduleImport()) {
 					return $this->save();
 			}
@@ -203,11 +215,11 @@ class Categories extends AppModel {
 
 	protected function scheduleImport()
 	{
-		if (!Import::check('import')) {
-			return false;
-		}
+		if (!Import::check('import')) return false;
+
 		$uploader = new FileUpload();
 		$uploader->path = APP_ROOT . '/public/uploads/imports';
+
 		try {
 			$importFile = $uploader->upload($this->data['import'], Security::hash(time()) . '_:original_name');
 
@@ -229,17 +241,16 @@ class Categories extends AppModel {
 			} else {
 				throw new Exception('Can\'t import file');
 			}
-
 		} catch (Exception $e) {
 			Session::writeFlash('error', s('Sorry, can\'t import category'.$e->getMessage()));
 			return false;
 		}
 	}
 
-	protected function sendImportMail($params = array()) {
-		if (!Config::read ( 'Mail.preventSending' )) {
-			require_once 'lib/mailer/Mailer.php';
-			$segment = Model::load ( 'Segments' )->firstById (MeuMobi::segment());
+	protected function sendImportMail($params = array())
+	{
+		if (!Config::read('Mail.preventSending')) {
+			$segment = Model::load('Segments')->firstById(MeuMobi::segment());
 			$user = Auth::user();
 			$default = array(
 				'user' => $user,
@@ -248,15 +259,15 @@ class Categories extends AppModel {
 			);
 			$data = array_merge($default, $params);
 
-			$mailer = new Mailer (array (
+			$mailer = new Mailer(array (
 				'from' => $segment->email,
-				'to' => array($user->email => $user->fullname ()),
+				'to' => array($user->email => $user->fullname()),
 				'subject' => $data['title'],
-				'views' => array ('text/html' => 'categories/confirm_import_mail.htm'),
+				'views' => array('text/html' => 'categories/confirm_import_mail.htm'),
 				'layout' => 'mail',
 				'data' => $data,
 			));
-			return $mailer->send ();
+			return $mailer->send();
 		}
 	}
 
@@ -301,7 +312,7 @@ class Categories extends AppModel {
 			'parent_id' => $id
 		)));
 
-		foreach($items as $item) {
+		foreach ($items as $item) {
 			Items::remove(array('_id' => $item->id()));
 		}
 	}
