e873df5a50c6d993552d683af37c9f67cf0a015c
juliogreff <juliogreff@gmail.com>
2013-03-19 12:51:27 -0300

    Enables GoogleMerchantFeed and MerchantProduct on fbiz segment

--- segments/fbiz/config.php
+++ segments/fbiz/config.php
@@ -1,12 +1,12 @@
 <?php
 
 Config::write('Segment', array(
-  'id' => 'fbiz',
-  'title' => 'f.biz',
-  'items' => array('articles', 'business'),
-  'extensions' => array('store-locator', 'rss'),
-  'root' => 'Index',
-  'email' => array('no-reply@fbiz.com.br' => 'f.biz'),
-  'hideCategories' => false,
-  'enableSignup' => false,
+	'id' => 'fbiz',
+	'title' => 'f.biz',
+	'items' => array('articles', 'business', 'merchant_products'),
+	'extensions' => array('store-locator', 'rss', 'google-merchant-feed'),
+	'root' => 'Index',
+	'email' => array('no-reply@fbiz.com.br' => 'f.biz'),
+	'hideCategories' => false,
+	'enableSignup' => false,
 ));
