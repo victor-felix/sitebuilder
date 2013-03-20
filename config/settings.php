92d171b46ef61e6e72042607f1ddb1e088cd99e9
juliogreff <juliogreff@gmail.com>
2013-03-20 16:26:32 -0300

    Remove multiInstance config flag

--- config/settings.php
+++ config/settings.php
@@ -18,7 +18,6 @@ Config::write('SiteLogos.resizes', array('200x200'));
 Config::write('SitePhotos.resizes', array('139x139#', '314x220'));
 Config::write('BusinessItems.resizes', array('80x60#', '80x80#', '139x139#', '314x220'));
 
-Config::write('multiInstances', 1);
 $dir = new GlobIterator(__DIR__ . '/segments/*.php');
 foreach($dir as $file) {
     if($file->isFile()) {
