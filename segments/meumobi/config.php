48d4024929358794df3e68149b97899351744ebc
juliogreff <juliogreff@gmail.com>
2013-04-03 14:06:27 -0300

    Enables products type for meumobi segment

--- segments/meumobi/config.php
+++ segments/meumobi/config.php
@@ -4,7 +4,7 @@ Config::write('Segment', array(
   'id' => 'meumobi',
   'title' => 'MeuMobi',
   'root' => 'Index',
-  'items' => array('articles', 'business'),
+  'items' => array('articles', 'business', 'products'),
   'extensions' => array('rss','store-locator'),
   'email' => array('no-reply@meumobi.com' => 'MeuMobi'),
   'hideCategories' => false,
