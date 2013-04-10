67fde12fb88bea03ee9ac7d35ee5ba6ea5bdd413
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-04-10 08:40:19 -0300

    fix encodec accents on items list

--- lib/helpers/BbcodeHelper.php
+++ lib/helpers/BbcodeHelper.php
@@ -2,14 +2,18 @@
 
 require_once 'lib/bbcode/Decoda.php';
 
-class BbcodeHelper extends Helper {
-    public function parse($text) {
-        $parser = new Decoda($text);
-        return $parser->parse(true);
-    }
+class BbcodeHelper extends Helper
+{
+	public function parse($text)
+	{
+			$parser = new Decoda($text);
+			return $parser->parse(true);
+	}
 
-    public function strip($text) {
-        $text = $this->parse($text);
-        return Sanitize::html($text, false);
-    }
+	public function strip($text)
+	{
+			$text = $this->parse($text);
+			$text = Sanitize::html($text, false);
+			return htmlspecialchars_decode($text);
+	}
 }
