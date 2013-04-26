d6b597312da6415016ba7d727a7763b14994a64d
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-04-26 09:01:17 -0300

    fix error layout

--- app/views/layouts/error.htm.php
+++ app/views/layouts/error.htm.php
@@ -5,25 +5,26 @@
 		<title>:( <?php echo s('Page not found') ?> | <?php echo MeuMobi::currentSegment()->title ?></title>
 		<meta name="robots" content="noindex, nofollow">
 		<link rel="shortcut icon" href="<?php echo Mapper::url("/images/layout/favicon.png") ?>" type="image/png" />
-		<?php echo $this->html->stylesheet('shared/register', 'shared/uikit') ?>
+		<?php echo $this->html->stylesheet('shared/base', 'shared/register', 'shared/uikit', 'segment') ?>
 	</head>
-
-	<body>
-		<div id="header">
-			<?php echo $this->html->link($this->html->image('layout/logo.png', array('alt'=> MeuMobi::currentSegment()->title)), '/', array('class'=>'logo')) ?>
-		</div>
-
-		<div id="content" style="background:none;-webkit-box-shadow: none;-moz-box-shadow: none;border: 0">
-			<div class="registration-finished" style="padding-bottom: 10px">
-				<?php echo $this->html->image('shared/layout/error.png', array(
-					'alt' => s('Page not found')
-				)) ?>
-				<h2><?php echo s('Page not found') ?></h2>
-				<div class="clear"></div>
+	<body class="error">
+		<div class="wrapper">
+			<div id="header">
+				<?php echo $this->html->link($this->html->image('layout/logo.png', array('alt'=> MeuMobi::currentSegment()->title)), '/', array('class'=>'logo')) ?>
 			</div>
+			<div class="content-wrapp">
+				<div id="content">
+					<div class="registration-finished">
+						<?php echo $this->html->image('shared/layout/error.png', array(
+							'alt' => s('Page not found')
+						)) ?>
+						<h2><?php echo s('Page not found') ?></h2>
+						<div class="clear"></div>
+					</div>
+				</div>
+			</div>
+			<?php echo $this->element("layouts/footer") ?>
 		</div>
-
-		<?php echo $this->element("layouts/footer") ?>
 		<?php echo $this->html->script('shared/jquery', 'shared/main') ?>
 		<?php echo $this->html->scriptsForLayout ?>
 	</body>
