c874007ae6e7d501ff7820ffb5b41609353269fa
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-05-08 15:38:37 -0300

    fix theme lsit

--- app/views/sites/_themes_list.htm.php
+++ app/views/sites/_themes_list.htm.php
@@ -21,7 +21,7 @@
 						<?php echo $this->element('sites/skins_list', array(
 							'skins' => $theme->skins(),
 							'currentSkin' => $theme->id() == $site->theme ? $site->skin() : null,
-							'customizeLink' => true,
+							'customizeLink' => (bool)$site->theme,
 						)) ?>
 					</li>
 				<?php endforeach ?>
