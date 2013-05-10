0342a1050a35bd8d73ae4ce2fa92690810c781e3
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2013-05-10 11:37:25 -0300

    clean code

--- lib/helpers/ItemsHelper.php
+++ lib/helpers/ItemsHelper.php
@@ -1,6 +1,7 @@
 <?php
 
-class ItemsHelper extends Helper {
+class ItemsHelper extends Helper
+{
 	protected $types = array(
 		'default' => array(
 			'type' => 'text',
@@ -29,22 +30,25 @@ class ItemsHelper extends Helper {
 	);
 	protected $item;
 
-	public function __construct($view)
+	public function __construct ($view)
 	{
 		parent::__construct($view);
-		$this->types['related'] = function($type) use ($view) {
+		$this->types['related'] = function  ($type) use( $view)
+		{
 			$classname = '\app\models\items\\' . $type[1];
 			$conditions = array(
 				'type' => Inflector::underscore($type[1]),
 				'site_id' => $view->controller->getCurrentSite()->id
 			);
-			$items = $classname::find('all', array('conditions' => $conditions, 'order' => 'title'));
+			$items = $classname::find('all', 
+				array(
+					'conditions' => $conditions,
+					'order' => 'title'
+				));
 			$options = array();
-
-			foreach($items as $item) {
+			foreach ($items as $item) {
 				$options[$item->id()] = $item->title;
 			}
-
 			return array(
 				'name' => 'related[]',
 				'type' => 'select',
@@ -55,46 +59,39 @@ class ItemsHelper extends Helper {
 		};
 	}
 
-	public function form($url, $item, $attr)
+	public function form ($url, $item, $attr)
 	{
 		$this->item = $item;
-
 		$attr += array(
 			'method' => 'file',
 			'object' => $item
 		);
-
 		return $this->form->create($url, $attr);
 	}
 
-	public function endform()
+	public function endform ()
 	{
 		return $this->form->close();
 	}
 
-	public function input($name)
+	public function input ($name)
 	{
 		$field = $this->item->field($name);
-
 		$defaults = array(
 			'label' => $field->title
 		);
 		$type = (array) $field->type;
 		$type = $type[0];
-
-		if(is_array($this->types[$type])) {
+		if (is_array($this->types[$type])) {
 			$type_attr = $this->types[$type];
-		}
-		else {
+		} else {
 			$type_attr = $this->types[$type]($field->type);
 		}
-
-		$params = (array)$field;
-		unset($params['type'],$params['title']);
-		
-		$attr = array_merge($defaults, $this->types['default'], $type_attr,$params);
+		$params = (array) $field;
+		unset($params['type'], $params['title']);
+		$attr = array_merge($defaults, $this->types['default'], $type_attr, 
+			$params);
 		$attr['class'] .= ' large';
-
 		return $this->form->input($name, $attr);
 	}
 }
