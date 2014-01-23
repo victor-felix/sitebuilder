d473e211a3174ab3d1cd5f28c180b76aaa11ade8
Tadeu Valentt <tadeu.valentt@gmail.com>
2014-01-23 11:50:31 -0300

    update sitebuilder

--- segments/enterprise/config.php
+++ segments/enterprise/config.php
@@ -3,7 +3,7 @@
 Config::write('Segment', array(
   'id' => 'enterprise',
   'title' => 'MeuMobi Enterprise',
-  'items' => array('articles', 'business', 'products', 'merchant_products', 'promotions'),
+  'items' => array('articles'),
   'extensions' => array('store-locator', 'rss', 'google-merchant-feed'),
   'root' => 'Index',
   'email' => array('no-reply@meumobi.com' => 'MeuMobi Enterprise'),
