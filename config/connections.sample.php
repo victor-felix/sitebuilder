68c3622a2fa180d985782084838e71828d3bf4bb
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-04-17 17:38:06 -0300

    add development connection

--- config/connections.sample.php
+++ config/connections.sample.php
@@ -1,53 +1,65 @@
 <?php
-
 use lithium\data\Connections;
 
-$mysql = array(
-    'rimobi' => array(
-        'driver' => 'MySql',
-        'host' => 'localhost',
-        'user' => 'meumobi',
-        'password' => 'q1T10Cr',
-        'database' => 'rimobi',
-        'prefix' => ''
-			),
-		'production' => array(
-        'driver' => 'MySql',
-        'host' => 'localhost',
-        'user' => 'root',
-        'password' => 'ipanemax2010',
-        'database' => 'meumobi_partners',
-        'prefix' => ''
-    ),
-    'integration' => array(
-        'driver' => 'MySql',
-        'host' => 'localhost',
-        'user' => 'meumobi',
-        'password' => 'q1T10Cr',
-        'database' => 'int_partners',
-        'prefix' => ''
-    )
+$mysql = array (
+		'rimobi' => array (
+				'driver' => 'MySql',
+				'host' => 'localhost',
+				'user' => 'meumobi',
+				'password' => 'q1T10Cr',
+				'database' => 'rimobi',
+				'prefix' => '' 
+		),
+		'production' => array (
+				'driver' => 'MySql',
+				'host' => 'localhost',
+				'user' => 'root',
+				'password' => 'ipanemax2010',
+				'database' => 'meumobi_partners',
+				'prefix' => '' 
+		),
+		'integration' => array (
+				'driver' => 'MySql',
+				'host' => 'localhost',
+				'user' => 'meumobi',
+				'password' => 'q1T10Cr',
+				'database' => 'int_partners',
+				'prefix' => ''
+		),
+		'development' => array (
+				'driver' => 'MySql',
+				'host' => 'localhost',
+				'user' => 'meumobi',
+				'password' => 'q1T10Cr',
+				'database' => 'dev_partners',
+				'prefix' => ''
+		),
 );
 
-$mongodb = array(
-    'rimobi' => array(
-        'type' => 'MongoDb',
-        'host' => 'localhost',
-        'database' => 'rimobi'
-			),
-		'production' => array(
-        'type' => 'MongoDb',
-        'host' => 'localhost',
-        'database' => 'meumobi_partners'
-    ),
-    'integration' => array(
-        'type' => 'MongoDb',
-        'host' => 'localhost',
-        'database' => 'int_partners'
-    )
+$mongodb = array (
+		'rimobi' => array (
+				'type' => 'MongoDb',
+				'host' => 'localhost',
+				'database' => 'rimobi' 
+		),
+		'production' => array (
+				'type' => 'MongoDb',
+				'host' => 'localhost',
+				'database' => 'meumobi_partners' 
+		),
+		'integration' => array (
+				'type' => 'MongoDb',
+				'host' => 'localhost',
+				'database' => 'int_partners' 
+		),
+		'integration' => array (
+				'type' => 'MongoDb',
+				'host' => 'localhost',
+				'database' => 'dev_partners'
+		)
 );
 
-$env = Config::read('App.environment');
-Connection::add($mysql);
-Connection::add('default', $mysql[$env]);
-Connections::add('default', $mongodb[$env]);
+$env = Config::read ( 'App.environment' );
+Connection::add ( $mysql );
+Connection::add ( 'default', $mysql [$env] );
+Connections::add ( 'default', $mongodb [$env] );
