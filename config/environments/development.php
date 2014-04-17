55b5b4c2466603a7e446a6ebb9d537badcbcd09f
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-04-17 17:29:20 -0300

    update development env

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,13 +1,13 @@
 <?php
 
-require dirname(dirname(__DIR__)) . '/sitebuilder/config/error_handler.php';
-
-ini_set('error_reporting', -1);
+ini_set('error_reporting', E_ALL ^ E_DEPRECATED);
 ini_set('display_errors', 'On');
 
 Config::write('Mail.preventSending', true);
 Config::write('Debug.showErrors', true);
 
 Config::write('Api.ignoreAuth', true);
+Config::write('Preview.url', 'http://placeholder.int-meumobi.com');
+Config::write('Sites.domain', 'int-meumobi.com');
 
 Config::write('Themes.url', 'http://meu-template-engine.int-meumobi.com/themes.json');
