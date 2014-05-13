3d8e90bf974ffbf91d0bf3ab54ce7d1185c2bf1b
victor.dias <victor.dias@ks393527.kimsufi.com>
2014-05-13 23:07:15 +0200

    udpate config of segment to only show required FieldSets

--- segments/enterprise/config.php
+++ segments/enterprise/config.php
@@ -3,13 +3,14 @@
 Config::write('Segment', array(
   'id' => 'enterprise',
   'title' => 'MeuMobi Enterprise',
-  'items' => array('articles'),
-  'extensions' => array('store-locator', 'rss', 'google-merchant-feed'),
+  'items' => array('articles', 'events'),
+  'extensions' => array('store-locator', 'event-feed', 'rss', 'google-merchant-feed'),
   'root' => 'Index',
   'email' => array('no-reply@meumobi.com' => 'MeuMobi Enterprise'),
   'hideCategories' => false,
-  'enableSignup' => false,
+  'enableSignup' => true,
   'primaryColor' => '#000',
   'analytics' => 'UA-22519238-3',
+  'themes' => array('paraty', 'posto9', 'leblon', 'helmut', 'flip', 'rimobi', 'flip_app', 'rimobi'),
   'enableFieldSet' => array('photos','weblinks','location', 'contact', 'news', 'description', 'timetable', 'stocks'),
 ));
