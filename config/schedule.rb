8e3cc06ac63162fb84eb9cdae0183366d7a83af6
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-06-03 10:50:56 -0300

    remove merchant_products scheduling cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,14 +21,6 @@ every 1.hour do
 end
 
 every 1.day do
-  command "php #{File.expand_path 'sitebuilder/script/update_merchant_products.php'} low"
-end
-
-every 1.minute do
-  command "php #{File.expand_path 'sitebuilder/script/update_merchant_products.php'} high"
-end
-
-every 1.day do
   command "php #{File.expand_path 'sitebuilder/script/update_events.php'} low"
 end
 
