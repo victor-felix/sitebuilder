1b92d65e56f43ac0b511c9519e3e4ce362ccc664
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-07-24 10:11:00 -0300

    add coors initializer

---
+++ config/initializers/coors.php
@@ -0,0 +1,11 @@
+<?php
+//Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers: Origin, X-Requested-With, Accept, X-Authentication-Token, Content-Type');
+    header("Content-Length: 0");
+    header("Content-Type: text/plain");
+		http_response_code(200);
+		exit;
+} 
