79b72759fd1db28a20450d03af853f0c2ad57ecf
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-08-05 11:55:41 -0300

    update coors initializer

--- config/initializers/coors.php
+++ config/initializers/coors.php
@@ -1,6 +1,6 @@
 <?php
 //Handle preflight requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
 		header('Access-Control-Allow-Headers: Origin, X-Requested-With, Accept, X-Authentication-Token, Content-Type');
