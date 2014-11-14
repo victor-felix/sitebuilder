7333d419898611dc45ff758e0b9c94cde7526bbc
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-11-14 15:26:28 -0300

    allow create plugins from json

--- script/create_visitors.php
+++ script/create_visitors.php
@@ -6,7 +6,26 @@ require dirname(__DIR__) . '/config/cli.php';
 
 $_ = array_shift($argv);
 
-$file = $argv[0];
+$file = @$argv[0];
+
+if (!$file) {
+	echo <<<EOL
+	File format:
+	[
+		{
+			"site_id" : 10, 
+			"email" : "visitor@mail.com",
+			"password" : "123456",
+			"groups" : ["group1", "group2"],
+			"devices" : []
+		},
+		...
+	]	
+
+EOL;
+	return;
+}
+
 $json = file_get_contents($file);
 $visitors = json_decode($json, 1);
 $repo = new VisitorsRepository();
