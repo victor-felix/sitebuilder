977f400a5e30e3468174cd3e9ccc8c48da624005
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-12-10 11:27:16 -0300

    fix Mailer bugs

--- lib/mailer/Mailer.php
+++ lib/mailer/Mailer.php
@@ -41,8 +41,9 @@ class Mailer {
         $message->setFrom($this->from);
         $message->setTo($this->to);
 
+        $view = new View();
         foreach($this->views as $type => $path):
-            $content = $view->render($type);
+            $content = $view->render($path, $this->data, $this->layout);
             $message->addPart($content, $type);
         endforeach;
 
