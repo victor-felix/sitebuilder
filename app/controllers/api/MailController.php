b22eabb6adf2473e7f6c4026c0f0054f13e753f1
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-12-11 13:05:06 -0300

    use segment as mail sender

--- app/controllers/api/MailController.php
+++ app/controllers/api/MailController.php
@@ -26,16 +26,16 @@ class MailController extends ApiController
 		I18n::locale($site->language);
 		$subject = s('%s Contact Mail', $site->title);
 		$response = [];
-
+		$segment = \MeuMobi::currentSegment();
 		$mailer = new Mailer(array(
-			'from' => array($this->request->get('data:mail') => $this->request->get('data:name')),
+			'from' => $segment->email,
 			'to' => array($site->email => $site->title),
 			'subject' => $subject,
 			'views' => array('text/html' => 'sites/contact_mail.htm'),
 			'layout' => 'mail',
 			'data' => array(
 				'site' => $site,
-				'segment' => \MeuMobi::currentSegment(),
+				'segment' => $segment,
 				'name' => $this->request->get('data:name'),
 				'mail' => $this->request->get('data:mail'),
 				'phone' => $this->request->get('data:phone'),
