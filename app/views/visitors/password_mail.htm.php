150455bbbcded26a19c01a498389729bcd0eee87
victor.dias <victor.dias@ipanemax.com>
2014-12-15 18:31:08 +0100

    update mail to visitors

--- app/views/visitors/password_mail.htm.php
+++ app/views/visitors/password_mail.htm.php
@@ -2,13 +2,13 @@
   Olá,
   <br />
   <br />
-  Você foi convidado para testar o Aplicativo Infobox de Comunicação Interna da Siemens. Através dele divulgaremos notícias importantes sobre a empresa, de forma fácil, privada e segura.
+  Você foi convidado para testar o Aplicativo Infobox de Comunicação Interna da Siemens. Por meio dele, divulgaremos notícias importantes sobre a empresa, de forma fácil, privada e segura.
   <br />
   <br /> 
   O App Infobox é compatível com smartphones Android e iOS. Acesse o link a seguir do seu celular e comece a testar: <a href="https://build.phonegap.com/apps/843971/install">https://build.phonegap.com/apps/843971/install</a>
   <br />
   <br />
-  ou usando o QR code seguinte:
+  Se preferir, faça o download usando o QR code seguinte:
   <br />
   <img class="qr-code" src="https://chart.googleapis.com/chart?chs=116x116&amp;cht=qr&amp;chl=https://build.phonegap.com/apps/843971/install/z7yxG25z65W7pzxAve5E&amp;chld=L|1&amp;choe=UTF-8">
   <br />
@@ -20,8 +20,12 @@
   <strong>senha:</strong> <?= $password ?>
   <br />
   <br />
-  Qualquer dúvida entre em contato com a equipe de comunicação interna: onlinecommunications.br@siemens.com
+  Qualquer dúvida, entre em contato com a equipe de comunicação interna: onlinecommunications.br@siemens.com
   <br />
   <br />
   Att,
+  <br />
+  <br />
+  Siemens Ltda.<br />
+  Communications and Government Affairs
 </p>
