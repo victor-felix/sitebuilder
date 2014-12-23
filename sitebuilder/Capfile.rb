f35a71678195ef6f4d4b96056ceb89708a4b7c60
juliogreff <juliogreff@gmail.com>
2014-12-23 21:49:16 +0100

    Points capistrano to use the new API docs

--- sitebuilder/Capfile.rb
+++ sitebuilder/Capfile.rb
@@ -48,7 +48,7 @@ namespace :deploy do
   end
 
   task :parse_api_doc do
-    run "ruby #{release_path}/sitebuilder/script/parse_docs.rb #{release_path}/sitebuilder/doc/api_tech_spec.md #{release_path}/segments/meumobi/public/docs/api.html 'MeuMobi: Tech Spec'"
+    run "ruby #{release_path}/sitebuilder/script/parse_docs.rb #{release_path}/doc/api_specs.md #{release_path}/segments/meumobi/public/docs/api.html 'MeuMobi: Tech Spec'"
   end
 
   task :platform_check do
