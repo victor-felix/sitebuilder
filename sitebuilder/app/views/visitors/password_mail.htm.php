f0ac76769382ef550d1c67832d50ed62d74575f1
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2015-01-06 11:28:03 -0300

    use visitor/footer on password email

--- sitebuilder/app/views/visitors/password_mail.htm.php
+++ sitebuilder/app/views/visitors/password_mail.htm.php
@@ -1,31 +1,22 @@
 <p>
-  Olá,
-  <br />
-  <br />
-  Você foi convidado para testar o Aplicativo Infobox de Comunicação Interna da Siemens. Por meio dele, divulgaremos notícias importantes sobre a empresa, de forma fácil, privada e segura.
-  <br />
-  <br /> 
-  O App Infobox é compatível com smartphones Android e iOS. Acesse o link a seguir do seu celular e comece a testar: <a href="https://build.phonegap.com/apps/843971/install">https://build.phonegap.com/apps/843971/install</a>
-  <br />
-  <br />
-  Se preferir, faça o download usando o QR code seguinte:
-  <br />
-  <img class="qr-code" src="https://chart.googleapis.com/chart?chs=116x116&amp;cht=qr&amp;chl=https://build.phonegap.com/apps/843971/install/z7yxG25z65W7pzxAve5E&amp;chld=L|1&amp;choe=UTF-8">
-  <br />
-  <br />
-  Suas informações de acesso são:
-  <br />
-  <strong>email:</strong> <?= $email ?>
-  <br />
-  <strong>senha:</strong> <?= $password ?>
-  <br />
-  <br />
-  Qualquer dúvida, entre em contato com a equipe de comunicação interna: onlinecommunications.br@siemens.com
-  <br />
-  <br />
-  Att,
-  <br />
-  <br />
-  Siemens Ltda.<br />
-  Communications and Government Affairs
+	Olá,
+	<br />
+	<br />
+	Você foi convidado para testar o Aplicativo Infobox de Comunicação Interna da Siemens. Por meio dele, divulgaremos notícias importantes sobre a empresa, de forma fácil, privada e segura.
+	<br />
+	<br /> 
+	O App Infobox é compatível com smartphones Android e iOS. Acesse o link a seguir do seu celular e comece a testar: <a href="https://build.phonegap.com/apps/843971/install">https://build.phonegap.com/apps/843971/install</a>
+	<br />
+	<br />
+	Se preferir, faça o download usando o QR code seguinte:
+	<br />
+	<img class="qr-code" src="https://chart.googleapis.com/chart?chs=116x116&amp;cht=qr&amp;chl=https://build.phonegap.com/apps/843971/install/z7yxG25z65W7pzxAve5E&amp;chld=L|1&amp;choe=UTF-8">
+	<br />
+	<br />
+	Suas informações de acesso são:
+	<br />
+	<strong>email:</strong> <?= $email ?>
+	<br />
+	<strong>senha:</strong> <?= $password ?>
+	<?= $this->element('visitors/footer') ?>
 </p>
