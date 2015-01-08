ba57a050384bcd29692ffe7153c0a35a362820d5
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2015-01-08 08:28:15 -0300

    update worker

--- sitebuilder/lib/meumobi/sitebuilder/WorkerManager.php
+++ sitebuilder/lib/meumobi/sitebuilder/WorkerManager.php
@@ -2,11 +2,13 @@
 
 namespace meumobi\sitebuilder;
 
+use app\models\Jobs;
+
 class WokerManager
 {
 	public static function enqueue($type, $params)
 	{
-		$job = \app\models\Jobs::create([
+		$job = Jobs::create([
 			'type' => $type,
 			'params' => $params
 		]);
@@ -15,17 +17,22 @@ class WokerManager
 
 	public static function execute($worker)
 	{
-		try {
-			$worker::perform();
-			self::destroy($worker);
-		} catch (Exception $e) {
-			//job failed
-		}
+		$worker::perform();
+		self::destroy($worker);
 	}
 
 	public static function getNextJobWorker()
 	{
-		
+		$job = Jobs::first(['order' => 'modified']);
+		if ($job) {
+			$workerClass = 'meumobi\sitebuilder\workers\\' . \Inflector::camelize($job->type);
+			return new $workerClass(compact('job'));
+		}
+	}
+
+	public static function destroy($worker)
+	{
+		Jobs::remove(['_id' => $worker->job->_id]);	
 	}
 }
 
