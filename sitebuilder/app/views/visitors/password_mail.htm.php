f937bc719706ab8fb2a3fe62229c10b1ccfd19ec
Tadeu Valentt <tadeu.valentt@gmail.com>
2015-04-14 12:29:16 -0300

    update visitors password email

--- sitebuilder/app/views/visitors/password_mail.htm.php
+++ sitebuilder/app/views/visitors/password_mail.htm.php
@@ -5,13 +5,23 @@
 	Você foi convidado para testar o Aplicativo Infobox de Comunicação Interna da Siemens. Por meio dele, divulgaremos notícias importantes sobre a empresa, de forma fácil, privada e segura.
 	<br />
 	<br /> 
-	O App Infobox é compatível com smartphones Android e iOS. Acesse o link a seguir do seu celular e comece a testar: <a href="https://build.phonegap.com/apps/843971/install">https://build.phonegap.com/apps/843971/install</a>
+	O App Infobox é compatível com smartphones Android e iOS. Através do celular, abra este email e click no link a seguir para iniciar a instalação: <a href="https://build.phonegap.com/apps/843971/install">https://build.phonegap.com/apps/843971/install</a>
 	<br />
 	<br />
 	Se preferir, faça o download usando o QR code seguinte:
 	<br />
 	<img class="qr-code" src="https://chart.googleapis.com/chart?chs=116x116&amp;cht=qr&amp;chl=https://build.phonegap.com/apps/843971/install/z7yxG25z65W7pzxAve5E&amp;chld=L|1&amp;choe=UTF-8">
 	<br />
+	<br />
+	Se encontrar dificuldades para instalar InfoBox Segue o passo a passo seguinte: 
+	<ul>
+		<li>
+			<?= $this->html->link(Mapper::url('/docs/Como_Instalar_InfoBox-iOS.pdf', true), 'Como instalar InfoBox no iOS') ?>
+		</li>
+		<li>
+			<?= $this->html->link(Mapper::url('/docs/Como_Instalar_InfoBox-Android.pdf', true), 'Como instalar InfoBox no Android') ?>
+		</li>
+	</ul>
 	<br />
 	Suas informações de acesso são:
 	<br />
