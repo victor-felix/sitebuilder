05a4308d2b8f7d04c288c17dad7525fc42ca460c
Tadeu Valentt <tadeu.valentt@gmail.com>
2015-07-16 18:25:36 -0300

    Add help documentation to import visitors script, closes #140
    Add script documentation to import visitors script to be displayed when
    the script is executed with invalid parameters.
    
    [*]: No backslash to call class, ie. AVOID "new \DOMXPath" (I recommend
    to run grep "new \" on files commited)
    [*]: Don't put the branch name in the title of the PR neither issue
    number.
    [*]: Updates on commits only concerns the target issue. None other
    updates should be pushed
    [*]: Each methods does one single task. AVOID multiple tasks on same
    method.
    [*]: Logs follow actions and are consistent with rest of platform
    [*]: All previous comments on exisiting code have been considered
    [*]: Always leave the last comma in a multi-line array (Zend Coding
    Style)
    [*]: ... and the most important, I've ran my code

--- sitebuilder/script/import_visitors.php
+++ sitebuilder/script/import_visitors.php
@@ -1,19 +1,37 @@
 <?php
-use meumobi\sitebuilder\repositories\VisitorsRepository;
-use meumobi\sitebuilder\entities\Visitor;
 use meumobi\sitebuilder\services\ImportVisitorsCsvService;
 
 require dirname(__DIR__) . '/config/cli.php';
 
-$options = getopt('f:s:', ['password-strategy::', 'exclusive', 'resend']);
+$options = getopt(null, ['site:', 'import-file:', 'password-strategy:', 'exclusive', 'resend']);
 
-$site = Model::load('Sites')->firstById($options['s']);
+//TODO use ParamsValidator after it be available on master
+if (isset($options['import-file']) && isset($options['site'])) {
+	$site = Model::load('Sites')->firstById($options['site']);
 
-require 'segments/' . $site->segment . '/config.php';
-$options['resend'] = isset($options['resend']);
-$import = new ImportVisitorsCsvService();
-if (isset($options['exclusive'])) $import->setMethod(ImportVisitorsCsvService::EXCLUSIVE);
-if (@$options['password-strategy']) $import->setPasswordStrategy($options['password-strategy']);
-$import->setSite($site);
-$import->setFile($options['f']);
-echo $import->import($options);
+	require 'segments/' . $site->segment . '/config.php';
+
+	$options['resend'] = isset($options['resend']);
+	$import = new ImportVisitorsCsvService();
+	if (isset($options['exclusive'])) $import->setMethod(ImportVisitorsCsvService::EXCLUSIVE);
+	if (isset($options['password-strategy'])) $import->setPasswordStrategy($options['password-strategy']);
+	$import->setSite($site);
+	$import->setFile($options['import-file']);
+	echo $import->import($options);
+} else {
+	echo <<<'EOL'
+usage: php import_visitors.php OPTIONS
+import site visitors from a csv file
+OPTIONS:
+	MANDATORY:
+		--import-file: csv file path
+		--site: site id
+	OPTIONAL:
+		--password-strategy default|random: if default the site slug will be used as password
+		--exclusive: removes previous visitors before import
+		--resend: send invite email to all imported visitors, including the already created ones
+EXAMPLE:
+$php sitebuilder/script/import_visitors.php --resend --exclusive --site 234 --import-file tmp/visitors.csv --password-strategy random
+
+EOL;
+}
