55ef613a28fa8ad8731c3ae2a7a60c84f3c11467
juliogreff <juliogreff@gmail.com>
2015-11-16 23:17:28 +0100

    Finishes php script successfully

--- sitebuilder/script/backup_database.php
+++ sitebuilder/script/backup_database.php
@@ -18,8 +18,8 @@ class Backup
 	}
 
 	protected static function getPath($file) {
-		
-		$path = dirname(__DIR__) . '/db/backups/'; 
+
+		$path = dirname(__DIR__) . '/db/backups/';
 		if(!file_exists($path))
 			mkdir($path, 0777, true);//create if necessary
 		return $path . $file;
@@ -27,7 +27,9 @@ class Backup
 }
 
 $mysqlConfig = Connection::config('default');
-$mongoConfig = Connections::get('default', ['config' => true]); 
+$mongoConfig = Connections::get('default', ['config' => true]);
 
 Backup::mysql($mysqlConfig);
 Backup::mongodb($mongoConfig);
+
+exit(0);
