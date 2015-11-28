d45a2021adc18f8a713c69521fb55a40fe631634
juliogreff <juliogreff@gmail.com>
2015-11-28 15:43:02 +0100

    Log events for command line scripts. Closes #273

--- sitebuilder/config/cli.php
+++ sitebuilder/config/cli.php
@@ -2,16 +2,25 @@
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
+use meumobi\sitebuilder\Logger;
+
 set_time_limit(0);
 
-ini_set('error_reporting', E_ALL & ~E_DEPRECATED & ~E_STRICT);
+ini_set('error_reporting', -1);
 ini_set('display_errors', 'On');
 
 function meumobi_lock($lock, $fn) {
 	$pidpath = APP_ROOT . "/tmp/{$lock}.pid";
 	$pidfile = fopen($pidpath, 'w+');
+	$log = ['lock' => $lock];
 
-	if (!flock($pidfile, LOCK_EX | LOCK_NB)) exit();
+	if (!flock($pidfile, LOCK_EX | LOCK_NB)) {
+		Logger::info('cli', 'cannot start cronjob. lock already acquired', $log);
+
+		exit();
+	}
+
+	Logger::info('cli', 'starting cronjob', $log);
 
 	fwrite($pidfile, getmypid());
 	fflush($pidfile);
@@ -20,4 +29,6 @@ function meumobi_lock($lock, $fn) {
 
 	fclose($pidfile);
 	unlink($pidpath);
+
+	Logger::info('cli', 'cronjob finished', $log);
 }
