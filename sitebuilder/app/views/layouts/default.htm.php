8e07c6dd10a5c94bd8ef0e6cd84869da807d4002
juliogreff <juliogreff@gmail.com>
2016-02-22 22:12:41 +0100

    Shows current site id on header

--- sitebuilder/app/views/layouts/default.htm.php
+++ sitebuilder/app/views/layouts/default.htm.php
@@ -72,7 +72,7 @@
 							<p class="business-name">
 								<span><?= s('You\'re currently editing') ?></span>
 								<?= $this->string->truncate(e($currentSite->title), 15) ?>
-								<span id="site-id">#<?= $site->id ?></span>
+								<span id="site-id">#<?= $currentSite->id ?></span>
 							</p>
 							<div class="right">
 								<p class="site-url dynamic-text" data-max-font-size="36">
