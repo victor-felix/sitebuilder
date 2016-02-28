4be02f787e8b874c35e78c78affe100df84eef82
juliogreff <juliogreff@gmail.com>
2016-02-28 11:38:05 +0100

    Returns an error response on internal errors

--- sitebuilder/config/initializers/error_handler.php
+++ sitebuilder/config/initializers/error_handler.php
@@ -1,22 +1,41 @@
 <?php
 
+use lithium\action\Response;
 use lithium\core\ErrorHandler;
 use meumobi\sitebuilder\Logger;
 
-ErrorHandler::apply(array('lithium\action\Dispatcher', 'run'),
-	array('type' => array(
+ErrorHandler::apply(
+	['lithium\action\Dispatcher', 'run'],
+	['type' => [
 		'app\models\sites\MissingSiteException',
 		'app\models\RecordNotFoundException',
 		'app\controllers\api\UnAuthorizedException',
 		'app\controllers\api\ForbiddenException',
 		'app\controllers\api\InvalidArgumentException',
 		'meumobi\sitebuilder\repositories\RecordNotFoundException'
-	)),
+	]],
 	function($exception, $params) {
-		$response = new \lithium\action\Response(array(
+		$response = new Response([
 			'status' => $exception['exception']->status,
 			'body' => json_encode(['error' => $exception['message']])
-		));
+		]);
+		echo $response->render();
+	}
+);
+
+ErrorHandler::apply(
+	['lithium\action\Dispatcher', 'run'],
+	['type' => ['Exception']],
+	function($exception, $params) {
+		$e = $exception['exception'];
+		Logger::error('sitebuilder', sprintf('uncaught exception %s: "%s" at %s line %s',
+			get_class($e), $e->getMessage(), $e->getFile(), $e->getLine()),
+			['exception' => $e]);
+
+		$response = new Response([
+			'status' => 500,
+			'body' => json_encode(['error' => 'internal server error'])
+		]);
 		echo $response->render();
 	}
 );
