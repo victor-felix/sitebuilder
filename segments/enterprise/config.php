0f072b923bfb20f53a1dab2a5ffcfe6aaf568247
Julio Greff <juliogreff@gmail.com>
2016-03-07 15:15:45 +0100

    Fixes syntax error. Closes #326

--- segments/enterprise/config.php
+++ segments/enterprise/config.php
@@ -19,6 +19,6 @@ Config::write('Segment', array(
   'analytics' => 'UA-22519238-3',
   'themes' => array('paraty', 'posto9', 'leblon', 'helmut', 'flip', 'rimobi', 'flip_app', 'rimobi'),
   'enableFieldSet' => array('photos','weblinks','location', 'contact', 'news', 'description', 'timetable', 'stocks'),
-  'enableApiAccessFromAllDomains' => true
+  'enableApiAccessFromAllDomains' => true,
   'domain' => 'enterprise.meumobilesite.com',
 ));
