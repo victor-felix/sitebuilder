8a7e77e0a19f11a921e89869cf7d860bdd02b699
victor dias <elbidone@gmail.com>
2016-07-08 20:36:56 -0300

    ENHANCEMENT: Closes #393, create segment Employee

---
+++ segments/employee/config.php
@@ -0,0 +1,20 @@
+<?php
+
+Config::write('Segment', array(
+  'id' => 'employee',
+  'title' => 'Employee',
+  'contactMail' => 'contact@meumobi.com',
+	'contactFacebook' => 'http://facebook.com/meumobi',
+	'contactTwitter' => 'http://twitter.com/meumobi',
+  'items' => array('articles', 'polls', 'events'),
+  'extensions' => array('rss'),
+  'root' => 'index',
+  'email' => array('contact@meumobi.com' => 'meumobi Employee'),
+  'hideCategories' => false,
+  'enableSignup' => false,
+  'fullOptions' => true,
+  'analytics' => 'UA-22519238-3',
+	'enableFieldSet' => array('visitors', 'weblinks', 'description', 'contact'),
+	'enableApiAccessFromAllDomains' => true,
+	'domain' => 'meumobi.com'
+));
