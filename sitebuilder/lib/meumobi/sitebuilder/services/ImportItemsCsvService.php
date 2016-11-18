edbd8233f05065e8d06fcf1d9bf1d5cf84d0021e
victor dias <elbidone@gmail.com>
2016-11-18 21:02:06 -0200

    FIX: Closes #437, import items by csv only save last entry (exclusive and inclusive)

--- sitebuilder/lib/meumobi/sitebuilder/services/ImportItemsCsvService.php
+++ sitebuilder/lib/meumobi/sitebuilder/services/ImportItemsCsvService.php
@@ -2,8 +2,13 @@
 
 namespace meumobi\sitebuilder\services;
 
-use lithium\data\Connections;
+use Model;
+use app\models\Items;
 use app\models\Jobs;
+use lithium\data\Connections;
+use meumobi\sitebuilder\Logger;
+use meumobi\sitebuilder\services\CreateItem;
+use meumobi\sitebuilder\services\UpdateItem;
 
 class ImportItemsCsvService extends ImportCsvService {
 	protected $category;
@@ -22,11 +27,11 @@ class ImportItemsCsvService extends ImportCsvService {
 				$log['total_items'] = $this->import();
 				$this->logger()->info('csv imported ', $log);
 			} catch (\Exception $e) {
-				$this->logger()->error('csv import error', [
-					'exception' => get_class($e),
-					'message' => $e->getMessage(),
-					'trace' => $e->getTraceAsString()
-				] + $log);
+			$this->logger()->error('csv import error', [
+				'exception' => get_class($e),
+				'message' => $e->getMessage(),
+				'trace' => $e->getTraceAsString()
+					] + $log);
 			}
 			$job->delete();
 		}
@@ -34,50 +39,92 @@ class ImportItemsCsvService extends ImportCsvService {
 		$this->logger()->info('csv import finished ', $stats);
 	}
 
+	function clearItems()
+	{
+		$category = $this->getCategory();
+  
+		Logger::info('items', 'removing category items', [
+			'category_id' => $category->id,
+			'site_id' => $category->site_id   
+				]);
+    
+		$category->removeItems();
+	}
+  
+	function getItem($data)
+	{
+		$item = null;
+		$category = $this->getCategory();
+
+		if (isset($data['id'])) {
+			$item = Items::find('first', [
+				'conditions' => [
+					'_id' => $data['id'],
+					'parent_id' => $category->id,
+					]
+			]);
+  
+			$item->set($data);
+		} else {
+			$classname = '\app\models\items\\' .
+				\Inflector::camelize($category->type);
+			$item =  $classname::create();
+
+			$data['parent_id'] = $category->id;
+			$data['site_id'] = $category->site_id;
+			$data['type'] = $category->type;
+
+			$item->set($data);
+		}
+
+		return $item;
+	}
+  
+	function createItem($item)
+	{
+		$service = new CreateItem();
+
+		return $service->create($item);
+	}
+
+	function updateItem($item)
+	{
+		$service = new UpdateItem();
+
+		return $service->update($item);
+	}
+
 	public function import()
 	{
-		$startTime = time();
 		$imported = 0;
-		$classname = '';
-		while ($item = $this->getNextItem()) {
-			$classname = '\app\models\items\\' .
-				\Inflector::camelize($this->getCategory()->type);
-			if (isset($item['id'])) {
-				$record = $classname::find('first', array(
-					'conditions' => array(
-						'_id' => $item['id']
-					),
-				));
-			}
-			if (!$record) {
-				$record = $classname::create();
-			}
 
-			$item['parent_id'] = $this->getCategory()->id;
-			$item['site_id'] = $this->getCategory()->site_id;
-			$item['type'] = $this->getCategory()->type;
-			$record->set($item);
-			$record->save();
-			$imported++;
+		Logger::info('items', 'start importing from csv');
+
+		// TODO: should clear items after to be sure that import will succeed
+		if (self::EXCLUSIVE == $this->method) {
+			$this->clearItems();
 		}
 
-		if (self::EXCLUSIVE == $this->method && $imported) {
-			//remove all items creates before import start
-			$classname::remove(
-				array(
-					'parent_id' => $this->getCategory()->id,
-					'created' => array(
-						'$lt' => new \MongoDate($startTime),
-					),
-				)
-			);
+		while ($data = $this->getNextItem()) {
+			$item = $this->getItem($data);
+
+			if ($item->id()) {
+				$this->updateItem($item);
+			} else {
+				$this->createItem($item);
+			}
+
+			$imported++; 
 		}
+
 		fclose($this->getFile());
 		unlink($this->filePath);
+		Logger::info('items', 'imported items from csv', ['total' => $imported]);
+
 		return $imported;
 	}
 
-	public function setCategory(\Categories $category)
+	public function setCategory($category)
 	{
 		$this->category = $category;
 	}
@@ -85,7 +132,7 @@ class ImportItemsCsvService extends ImportCsvService {
 	public function getCategory()
 	{
 		if (!$this->category) {
-			throw new \Exception("category not set");
+			throw new Exception("category not set");
 		}
 		return $this->category;
 	}
